096c2449d37d574c9c038048975b31781e22fe7b
Logan Davidson <logan.davidson@autorek.com>
2022-09-25 16:37:51 +0100

    Static file: CSS

--- rorsite/settings.py
+++ rorsite/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = 'static'
+STATIC_ROOT = 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
