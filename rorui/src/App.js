c026d3a5a0c94db0a7a27b4446b0746141ac639e
Logan Davidson <logan.davidson@autorek.com>
2022-10-08 21:45:30 +0100

    Make the React UI consume the DRF API

--- rorui/src/App.js
+++ rorui/src/App.js
@@ -1,34 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react';
 
-const games = [
-  {
-      "name": "game_a"
-  },
-  {
-      "name": "game_b"
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      gamesList: []
+    };
   }
-]
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  async componentDidMount() {
+    try {
+      const res = await fetch('http://localhost:8000/api/games/');
+      const gamesList = await res.json();
+      this.setState({
+        gamesList
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  renderGames = () => {
+    return this.state.gamesList.map(game => (<li key={game.name}>{game.name}</li>))
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>React UI</h1>
+        <p>List of games:</p>
+        <ul>
+          {this.renderGames()}
+        </ul>
+      </div>
+    )
+  }
 }
 
 export default App;
