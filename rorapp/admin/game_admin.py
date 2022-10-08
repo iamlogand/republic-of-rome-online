6d289b9bada7c134793c156f27368053566b16af
Logan Davidson <logan.davidson@autorek.com>
2022-10-08 08:36:56 +0100

    Add Game model with model and admin package directories

---
+++ rorapp/admin/game_admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from rorapp.models import Game
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ("name",)
---
+++ rorapp/models/game.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class Game(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
