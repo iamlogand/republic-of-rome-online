26bb402b752a348cb449cd30b3d0df8c9e1a242d
Logan Davidson <logan.davidson@autorek.com>
2022-11-02 16:02:56 +0000

    Add space for second form in sign in page

--- frontend/src/pages/SignInPage.js
+++ frontend/src/pages/SignInPage.js
@@ -5,10 +5,18 @@ export default function SignInPage(props) {
   return (
     <div className="autharea">
       <div>
-        <div className="box">
-          <div className="title">Sign In</div>
-          <SignInForm
-            setAuthData={props.setAuthData} />
+        <div>
+          <div className="box">
+            <div className="title">Sign In</div>
+            <SignInForm setAuthData={props.setAuthData} />
+          </div>
+        </div>
+        <div>
+          <div className="box">
+            <div className="title">Register</div>
+            <div className="title">Register TODO</div>
+            <SignInForm setAuthData={props.setAuthData} />
+          </div>
         </div>
       </div>
     </div>
