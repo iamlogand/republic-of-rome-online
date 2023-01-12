ed281a63a106573b81881ac35d31b348d6e0f44e
Logan Davidson <iamlogandavidson@gmail.com>
2023-01-12 22:01:21 +0000

    Add senator portrait

--- frontend/src/components/SenatorPortrait/index.js
+++ frontend/src/components/SenatorPortrait/index.js
@@ -1,15 +1,17 @@
 import { Component } from 'react';
 import "./SenatorPortrait.css";
+import Cornelius from "../../images/portraits/Cornelius.png";
 import RomeConsulIcon from "../../images/icons/RomeConsulIcon.png";
 
-class SignInForm extends Component {
+class SenatorPortrait extends Component {
     render() {
         return (
             <div className='senator-portrait'>
-                <img className='senator-portrait_major-office' src={RomeConsulIcon} alt="RomeConsul" width="30" height="30" />
+                <img className='senator-portrait_picture' src={Cornelius} alt="RomeConsul" width="72" height="72" />
+                <img className='senator-portrait_major-office' src={RomeConsulIcon} alt="RomeConsul" width="26" height="26" />
             </div>
         )
     }
 }
 
-export default SignInForm;
+export default SenatorPortrait;
