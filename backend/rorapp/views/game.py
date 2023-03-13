be51adc1dfd05a55c9669abb4c04e25e0d1b6641
Logan Davidson <iamlogandavidson@gmail.com>
2023-03-13 21:30:45 +0000

    API change to expose more `game` attributes

--- backend/rorapp/views/game.py
+++ backend/rorapp/views/game.py
@@ -9,6 +9,12 @@ class GameViewSet(viewsets.ModelViewSet):
     Retrieve all games.
     """
 
-    queryset = Game.objects.all().order_by('name')
+    queryset = Game.objects.all().order_by('-creation_date')
     serializer_class = GameSerializer
     permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        
+        # Prefetch the owner's username to eliminate the need to make additional queries to the database
+        return queryset.prefetch_related('owner')
--- backend/rorapp/views/user.py
+++ backend/rorapp/views/user.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets
 from django.contrib.auth.models import User
-from rorapp.serializers import UserSerializer
+from rorapp.serializers import UserSerializer, UserDetailSerializer
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 
@@ -15,4 +15,17 @@ class UserViewSet(viewsets.ViewSet):
     def list(self, request):
         queryset = User.objects.all()
         serializer = UserSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+
+class UserDetailViewSet(viewsets.ViewSet):
+    """
+    Retrieve the username and email of the current user.
+    """
+
+    permission_classes = [IsAuthenticated]
+
+    def list(self, request):
+        user = User.objects.get(username=request.user.username)
+        serializer = UserDetailSerializer(user)
         return Response(serializer.data)
