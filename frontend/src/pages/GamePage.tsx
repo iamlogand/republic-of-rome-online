9440a1a26c1a28619480c9a2b17a39343bcad315
Logan Davidson <iamlogandavidson@gmail.com>
2023-03-13 19:30:55 +0000

    Add praenomens (#31)
    * Add short praenomen to the sample JSON
    
    * Add praenomens

--- frontend/src/pages/GamePage.tsx
+++ frontend/src/pages/GamePage.tsx
@@ -23,6 +23,7 @@ const GamePage = (props: GamePageProps) => {
       let senators: Senator[] = [];
       for (let i = 0; i < objects.length; i++) {
         const senator: Senator = new Senator(
+          objects[i].praenomen,
           objects[i].name,
           objects[i].alive,
           objects[i].faction,
@@ -39,34 +40,28 @@ const GamePage = (props: GamePageProps) => {
     <div id="page_container">
       <TopBar username={props.username} />
       <div id="wide_page">
-        {senators.length > 0 &&
           <div id="page_content">
-            <h1>Republic of Rome Online</h1>
-            <div>
-              <Link to="/">Back to Main Menu</Link>
-            </div>
-              <h2>Examples of the "Senator Name" component</h2>
-              <p>
-                Sometimes senators may be referred to by name within a body of text, for example:
-                The leader of the Cyan faction is called <SenatorName senator={senators[7]} setInspectorRef={setInspectorRef} />. This <SenatorName senator={senators[7]} setInspectorRef={setInspectorRef} /> is joined by <SenatorName senator={senators[8]} setInspectorRef={setInspectorRef} />, <SenatorName senator={senators[9]} setInspectorRef={setInspectorRef} />, <SenatorName senator={senators[10]} setInspectorRef={setInspectorRef} /> again, <SenatorName senator={senators[11]} setInspectorRef={setInspectorRef} /> the Censor
-                and <SenatorName senator={senators[12]} setInspectorRef={setInspectorRef} />.
-                If only I had added more than four senators!</p>
-              <p>There are only three senators in the red faction:</p>
-              <ul>
-                <li><SenatorName senator={senators[0]} setInspectorRef={setInspectorRef} /></li>
-                <li><SenatorName senator={senators[1]} setInspectorRef={setInspectorRef} /></li>
-                <li><SenatorName senator={senators[2]} setInspectorRef={setInspectorRef} /></li>
-              </ul>
-              <p>Some senators, such as <SenatorName senator={senators[21]} setInspectorRef={setInspectorRef} /> and <SenatorName senator={senators[22]} setInspectorRef={setInspectorRef} /> are dead. Others like <SenatorName senator={senators[18]} setInspectorRef={setInspectorRef} /> and <SenatorName senator={senators[19]} setInspectorRef={setInspectorRef} /> are unaligned.</p>
-            <h2>Examples of the "Senator Portrait" component </h2>
-            <div className="container">
-              {senators.map((senator, index) => <SenatorPortrait 
-                key={index}
-                senator={senator}
-                setInspectorRef={setInspectorRef} /> )}
-            </div>
+            <h1>Some UI components</h1>
+            <Link to="/">Back to Main Menu</Link>
+            {senators.length > 0 &&
+              <div>
+                <h2>Senator names</h2>
+                <ul>
+                  {senators.map((senator, index) =>
+                    <li key={index}>
+                      Senator {index + 1}: <SenatorName senator={senator} setInspectorRef={setInspectorRef} />
+                    </li>
+                  )}
+                </ul>
+                <h2>Senator portraits</h2>
+                <div className="container">
+                  {senators.map((senator, index) =>
+                    <SenatorPortrait key={index} senator={senator} setInspectorRef={setInspectorRef} />
+                  )}
+                </div>
+              </div>
+            }
           </div>
-        }
       </div>
       {inspectorRef && <SenatorInspector {...inspectorRef} />}
     </div>
