fad779f141f3143b7fc90b52c3af9cd4b2ffe991
Logan Davidson <iamlogandavidson@gmail.com>
2023-03-25 14:07:53 +0000

    Make URI patterns and route names conventional
    Pluralize the token URI pattern and singularize the names of the games and users routes. This change makes the names more conventional.

--- backend/rorapp/urls.py
+++ backend/rorapp/urls.py
@@ -4,14 +4,14 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from rorapp import views
 
 router = routers.DefaultRouter()
-router.register('games', views.GameViewSet, basename='games')
-router.register('users', views.UsersViewSet, basename='users')
+router.register('games', views.GameViewSet, basename='game')
+router.register('users', views.UsersViewSet, basename='user')
 
 app_name = "rorapp"
 
 urlpatterns = [
     path('api/', include(router.urls)),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/token/email/', views.TokenObtainPairByEmailView.as_view(), name='token_obtain_pair_by_email')
+    path('api/tokens/', TokenObtainPairView.as_view(), name='tokens'),
+    path('api/tokens/refresh/', TokenRefreshView.as_view(), name='tokens-refresh'),
+    path('api/tokens/email/', views.TokenObtainPairByEmailView.as_view(), name='tokens-email')
 ]
