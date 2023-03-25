74387dec53d9ca87d33dd6dd137456c0e3353118
Logan Davidson <iamlogandavidson@gmail.com>
2023-03-25 13:38:06 +0000

    Rename and reorder API paths, names and imports
    Rename and reorder API paths, names and imports to improve readability of the code.

--- backend/rorapp/urls.py
+++ backend/rorapp/urls.py
@@ -1,18 +1,17 @@
 from django.urls import path, include
 from rest_framework import routers
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from rorapp import views
-from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView)
 
 router = routers.DefaultRouter()
-router.register('games', views.GameViewSet, basename='game')
+router.register('games', views.GameViewSet, basename='games')
 router.register('users', views.UsersViewSet, basename='users')
 
 app_name = "rorapp"
+
 urlpatterns = [
     path('api/', include(router.urls)),
-
-    # JWT authentication URLs
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/email/', views.TokenObtainPairByEmailView.as_view(), name='token_obtain_pair_by_email'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh')
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/email/', views.TokenObtainPairByEmailView.as_view(), name='token_obtain_pair_by_email')
 ]
