a98d9d0440cd5e4af7cfea0990a77c0479803d41
Logan Davidson <iamlogandavidson@gmail.com>
2023-03-25 13:40:09 +0000

    Set token serializer field type to password
    Set the obtain token by email serializer password field type to password, so it's corresponding form looks better in the web browsable API.

--- backend/rorapp/serializers/token.py
+++ backend/rorapp/serializers/token.py
@@ -3,4 +3,4 @@ from rest_framework import serializers
 
 class TokenObtainPairByEmailSerializer(serializers.Serializer):
     email = serializers.EmailField()
-    password = serializers.CharField()
+    password = serializers.CharField(style={'input_type': 'password'})
