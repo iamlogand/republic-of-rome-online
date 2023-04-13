bab301c0b8bf971549f20c799f2055dc2500fc8f
Logan Davidson <iamlogandavidson@gmail.com>
2023-04-13 23:07:34 +0100

    Add backend index page

--- backend/rorapp/views/__init__.py
+++ backend/rorapp/views/__init__.py
@@ -1,3 +1,4 @@
+from .index import index
 from .game import GameViewSet
 from .token import TokenObtainPairByEmailView
 from .user import UsersViewSet
--- backend/rorsite/urls.py
+++ backend/rorsite/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
 from django.contrib import admin
+from rorapp.views import index
 
 urlpatterns = [
+    path('', index),
     path('rorapp/', include('rorapp.urls')),
     path('admin/', admin.site.urls)
 ]
