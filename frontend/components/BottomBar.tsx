492c094e36fb10bd89a87586817e948319504c73
Logan Davidson <iamlogandavidson@gmail.com>
2023-04-15 17:02:16 +0100

    Add a bottom bar
    Add a bottom bar containing some info about IP rights.

---
+++ frontend/components/BottomBar.tsx
@@ -0,0 +1,11 @@
+import styles from './BottomBar.module.css';
+
+const BottomBar = () => {
+  return (
+    <footer className={styles.bottomBar} >
+      <i>Republic of Rome Online</i> is a free fan-made adaptation. We do not hold the intellectual property rights to <i>The Republic of Rome</i>.
+    </footer>
+  )
+}
+
+export default BottomBar;
