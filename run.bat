741053a1d1cd2c571c3ef981591c315251286a39
Logan Davidson <iamlogandavidson@gmail.com>
2023-04-18 01:08:12 +0100

    Delete run.bat

--- run.bat
+++
@@ -1,3 +0,0 @@
-set pythonVirtualEnvActivationScriptPath=".\backend\env\Scripts\activate"
-start cmd /k %pythonVirtualEnvActivationScriptPath%" & py .\backend\manage.py runserver"
-start cmd /k "cd .\frontend & npm run start"
