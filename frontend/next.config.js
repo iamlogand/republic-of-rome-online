869c5931b0d063d1fa0d05e59fe92b715ee6f02a
Logan Davidson <iamlogandavidson@gmail.com>
2023-05-29 10:38:12 +0100

    Add the AbortController npm package

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
+
+if (typeof globalThis.AbortController === "undefined") {
+  globalThis.AbortController = require("abort-controller");
+}
+
 const nextConfig = {
   reactStrictMode: true,
 }
