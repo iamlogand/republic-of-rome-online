0b0636a61dabcca5f126b04aa3ea40c24e48c0bb
Logan Davidson <iamlogandavidson@gmail.com>
2023-06-04 21:54:29 +0100

    Update SSL credential retrieval to use the AWS CLI

--- backend/start_prod.sh
+++ backend/start_prod.sh
@@ -11,9 +11,9 @@ rm /etc/nginx/sites-enabled/default
 cp nginx.conf /etc/nginx/sites-enabled/
 
 # Copy the SSL certificates from the Docker secrets
-cp /run/secrets/api_roronline_com.crt /etc/nginx/ssl/api_roronline_com.crt
-cp /run/secrets/certificate.key /etc/nginx/ssl/certificate.key
-cp /run/secrets/ca_bundle.crt /etc/nginx/ssl/ca_bundle.crt
+aws s3 cp s3://api-roronline-com-ssl/api_roronline_com.crt /etc/nginx/ssl/api_roronline_com.crt
+aws s3 cp s3://api-roronline-com-ssl/certificate.key /etc/nginx/ssl/certificate.key
+aws s3 cp s3://api-roronline-com-ssl/ca_bundle.crt /etc/nginx/ssl/ca_bundle.crt
 
 # Start Nginx and Gunicorn
 service nginx start && gunicorn rorsite.wsgi:application --bind 0.0.0.0:8000
