10d6ebfc53faf6f846d527f0362c24f7ee7eface
Logan Davidson <iamlogandavidson@gmail.com>
2023-06-18 18:39:06 +0100

    Setup Daphne and WebSockets for Docker Compose

--- backend/server/start.sh
+++ backend/server/start.sh
@@ -18,5 +18,5 @@ rm /etc/nginx/sites-enabled/default
 # Copy the Nginx configuration file
 cp server/nginx.conf /etc/nginx/sites-enabled/
 
-# Start Nginx and Gunicorn
-service nginx start && gunicorn rorsite.wsgi:application --bind 0.0.0.0:8000
+# Start Nginx and Daphne
+service nginx start && daphne rorsite.asgi:application --bind 0.0.0.0 --port 8000
