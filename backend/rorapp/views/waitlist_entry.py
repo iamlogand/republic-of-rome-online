9f3686b806063c2636b4e9537a9011384ff66c02
Logan Davidson <iamlogandavidson@gmail.com>
2023-07-02 13:43:41 +0100

    Fix whitespace issue in waitlist_entry.py

--- backend/rorapp/views/waitlist_entry.py
+++ backend/rorapp/views/waitlist_entry.py
@@ -1,22 +1,23 @@
 from rest_framework import viewsets
 from rorapp.models import WaitlistEntry
 from rorapp.serializers import WaitlistEntryCreateSerializer
+from rest_framework.exceptions import MethodNotAllowed
 
 class WaitlistEntryViewSet(viewsets.ModelViewSet):
-	queryset = WaitlistEntry.objects.all()
-	serializer_class = WaitlistEntryCreateSerializer
-
-	def list(self, request, *args, **kwargs):
+    queryset = WaitlistEntry.objects.all()
+    serializer_class = WaitlistEntryCreateSerializer
+    
+    def list(self, request, *args, **kwargs):
         raise MethodNotAllowed('GET')
-
+    
     def retrieve(self, request, *args, **kwargs):
         raise MethodNotAllowed('GET')
-
-	def destroy(self, request, *args, **kwargs):
+    
+    def destroy(self, request, *args, **kwargs):
         raise MethodNotAllowed('DELETE')
-
+    
     def update(self, request, *args, **kwargs):
         raise MethodNotAllowed('PUT')
-
+    
     def partial_update(self, request, *args, **kwargs):
         raise MethodNotAllowed('PATCH')
