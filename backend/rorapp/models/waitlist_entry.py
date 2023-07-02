4d292d1298d8ee62106890f25846ac2127e948c8
Sebastian Hirsch <4649153+SebastianHirsch@users.noreply.github.com>
2023-07-02 14:18:38 +0200

    Overwrite to disable methods on waitlist entry view

--- backend/rorapp/models/waitlist_entry.py
+++ backend/rorapp/models/waitlist_entry.py
@@ -4,3 +4,6 @@ from django.utils import timezone
 class WaitlistEntry(models.Model):
 	email = models.EmailField(max_length=254, unique=True)
 	entry_date = models.DateTimeField(default=timezone.now)
+
+	class Meta:
+		verbose_name_plural = "waitlist entries"
--- backend/rorapp/views/waitlist_entry.py
+++ backend/rorapp/views/waitlist_entry.py
@@ -5,3 +5,18 @@ from rorapp.serializers import WaitlistEntryCreateSerializer
 class WaitlistEntryViewSet(viewsets.ModelViewSet):
 	queryset = WaitlistEntry.objects.all()
 	serializer_class = WaitlistEntryCreateSerializer
+
+	def list(self, request, *args, **kwargs):
+        raise MethodNotAllowed('GET')
+
+    def retrieve(self, request, *args, **kwargs):
+        raise MethodNotAllowed('GET')
+
+	def destroy(self, request, *args, **kwargs):
+        raise MethodNotAllowed('DELETE')
+
+    def update(self, request, *args, **kwargs):
+        raise MethodNotAllowed('PUT')
+
+    def partial_update(self, request, *args, **kwargs):
+        raise MethodNotAllowed('PATCH')
