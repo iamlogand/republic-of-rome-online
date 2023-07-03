272552846dd8e906ff42d20038063c70d526e317
Sebastian Hirsch <4649153+SebastianHirsch@users.noreply.github.com>
2023-07-03 21:17:29 +0200

    Waitlist entry model sends success status if entry already exists or if it was created

--- backend/rorapp/views/waitlist_entry.py
+++ backend/rorapp/views/waitlist_entry.py
@@ -1,12 +1,25 @@
 from rest_framework import viewsets
+from rest_framework import status
 from rorapp.models import WaitlistEntry
 from rorapp.serializers import WaitlistEntryCreateSerializer
 from rest_framework.exceptions import MethodNotAllowed
+from rest_framework.response import Response
 
 class WaitlistEntryViewSet(viewsets.ModelViewSet):
     queryset = WaitlistEntry.objects.all()
     serializer_class = WaitlistEntryCreateSerializer
     
+    def create(self, request):
+        data = request.data
+        entry, created = WaitlistEntry.objects.get_or_create(**data)
+
+        serializer = self.serializer_class(entry)
+
+        if created:
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
     def list(self, request, *args, **kwargs):
         raise MethodNotAllowed('GET')
     
