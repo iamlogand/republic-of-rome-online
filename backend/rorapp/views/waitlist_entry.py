49113789581156ae2c46b9abc3af93ff228a4091
Logan Davidson <iamlogandavidson@gmail.com>
2023-07-04 12:42:32 +0100

    Add throttling to the `WaitlistEntryViewSet`

--- backend/rorapp/views/waitlist_entry.py
+++ backend/rorapp/views/waitlist_entry.py
@@ -2,10 +2,15 @@ from rest_framework import viewsets
 from rorapp.models import WaitlistEntry
 from rorapp.serializers import WaitlistEntryCreateSerializer
 from rest_framework.exceptions import MethodNotAllowed
+from rest_framework.throttling import UserRateThrottle
+
+class CustomRateThrottle(UserRateThrottle):
+    rate = "10/hour"
 
 class WaitlistEntryViewSet(viewsets.ModelViewSet):
     queryset = WaitlistEntry.objects.all()
     serializer_class = WaitlistEntryCreateSerializer
+    throttle_classes = [CustomRateThrottle]
     
     def list(self, request, *args, **kwargs):
         raise MethodNotAllowed('GET')
