150a13f14d27868d9c0d40b6a43312e5dbf5af32
Logan Davidson <iamlogandavidson@gmail.com>
2023-07-07 00:22:42 +0100

    Enable `start_game` to setup the game
    Flesh out the `start_game` ViewSet method so that it is capable of actually creating factions and senators, and assigning these to participants. There is no way to see these senators through the UI yet, but the results are browsable in the Django Admin site.

--- backend/rorapp/views/start_game.py
+++ backend/rorapp/views/start_game.py
@@ -1,13 +1,18 @@
+import os
+import json
+from django.conf import settings
+from django.utils import timezone
+from django.db import transaction
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from django.utils import timezone
-from rorapp.models import Game, GameParticipant
+from rorapp.models import Game, GameParticipant, Faction, FamilySenator
 
 
 class StartGameViewset(viewsets.ViewSet):
     
     @action(detail=True, methods=['post'])
+    @transaction.atomic
     def start_game(self, request, pk=None):
         
         # Try to get the game
@@ -28,6 +33,36 @@ class StartGameViewset(viewsets.ViewSet):
         participants = GameParticipant.objects.filter(game__id=game.id)
         if participants.count() < 3:
             return Response({"message": "Game must have at least 3 players to start"}, status=403)
+        
+        # Create factions
+        position = 1
+        for participant in participants.order_by('?'):
+            faction = Faction(game=game, position=position, player=participant)
+            faction.save()
+            position += 1
+        
+        # Build a deck of family senators
+        senator_json_path = os.path.join(settings.BASE_DIR, 'rorapp', 'presets', 'family.json')
+        with open(senator_json_path, 'r') as file:
+            senators_json = json.load(file)
+        for senator_name, senator_data in senators_json.items():
+            senator = FamilySenator(name=senator_name, game=game)
+            senator.save()
+        
+        # Draw senators
+        senators = FamilySenator.objects.filter(game__exact=game.id)
+        factions = Faction.objects.filter(game__exact=game)
+        required_senator_count = factions.count() * 3
+        drawn_senators = senators.order_by('?')[:required_senator_count]
+        
+        # Assign senators to factions
+        assignable_senators = drawn_senators
+        for faction in factions:
+            faction_senators = assignable_senators[:3]
+            assignable_senators = assignable_senators[3:]
+            for senator in faction_senators:
+                senator.faction = faction
+                senator.save()
         
         # Start the game
         game.start_date = timezone.now()
