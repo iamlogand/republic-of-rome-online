12ef2a57332d31985fc2b172560cd43cb4552ac5
Logan Davidson <iamlogandavidson@gmail.com>
2023-07-09 12:41:25 +0100

    Add ID field to user API serializers

--- backend/rorapp/serializers/user.py
+++ backend/rorapp/serializers/user.py
@@ -5,10 +5,10 @@ from django.contrib.auth.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('username',)
+        fields = ('id', 'username',)
 
 
 class UserDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('username', 'email')
+        fields = ('id', 'username', 'email')
