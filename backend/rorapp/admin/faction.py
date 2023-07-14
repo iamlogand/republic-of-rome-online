cd717bd3c14ee9b9a42b2da24cfae2972b37618b
Logan Davidson <iamlogandavidson@gmail.com>
2023-07-14 14:29:14 +0100

    Add id field to admin page
    Add id field and comments to the Faction admin page. The comments are to improve readability.

--- backend/rorapp/admin/faction.py
+++ backend/rorapp/admin/faction.py
@@ -2,12 +2,14 @@ from django.contrib import admin
 from rorapp.models import Faction, FamilySenator
 
 
+# Inline list showing related Senators
 class FamilySenatorInline(admin.TabularInline):
     model = FamilySenator
     extra = 0
     
 
+# Admin configuration for Faction
 @admin.register(Faction)
 class Faction(admin.ModelAdmin):
-    list_display = ('game', 'position', 'player')
+    list_display = ('id', 'game', 'position', 'player')
     inlines = [FamilySenatorInline]
