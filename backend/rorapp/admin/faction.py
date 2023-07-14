3ff2ffb202bedb65fc4a3b1db33ee16d4132e714
Logan Davidson <iamlogandavidson@gmail.com>
2023-07-14 14:34:49 +0100

    Add id fields and code comments to all admin code

--- backend/rorapp/admin/faction.py
+++ backend/rorapp/admin/faction.py
@@ -2,13 +2,13 @@ from django.contrib import admin
 from rorapp.models import Faction, FamilySenator
 
 
-# Inline list showing related Senators
+# Inline table showing related senators
 class FamilySenatorInline(admin.TabularInline):
     model = FamilySenator
     extra = 0
     
 
-# Admin configuration for Faction
+# Admin configuration for factions
 @admin.register(Faction)
 class Faction(admin.ModelAdmin):
     list_display = ('id', 'game', 'position', 'player')
--- backend/rorapp/admin/family_senator.py
+++ backend/rorapp/admin/family_senator.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from rorapp.models import FamilySenator
 
+
+# Admin configuration for family senators
 @admin.register(FamilySenator)
 class FamilySenator(admin.ModelAdmin):
-    list_display = ('name', 'game', 'faction')
+    list_display = ('id', 'name', 'game', 'faction')
