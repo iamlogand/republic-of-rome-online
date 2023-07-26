8ed1e53723c433a4bd59a401312c356cc3daeadb
Logan Davidson <iamlogandavidson@gmail.com>
2023-07-26 20:00:29 +0100

    Game play page show 404 on step 0
    Make the game play page show a 404 if the game hasn't started (hasn't reached step 1).

--- frontend/pages/games/[id]/play.tsx
+++ frontend/pages/games/[id]/play.tsx
@@ -78,7 +78,7 @@ const PlayGamePage = (props: PlayGamePageProps) => {
   // Render page error if user is not signed in
   if (user === null || props.authFailure) {
     return <PageError statusCode={401} />;
-  } else if (game === null) {
+  } else if (game === null || game.step < 1) {
     return <PageError statusCode={404} />
   }
 
