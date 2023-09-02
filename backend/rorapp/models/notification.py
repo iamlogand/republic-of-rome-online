33f2086cd0f9bc7ed3e205b0dcb6d8878f789df6
Logan Davidson <iamlogandavidson@gmail.com>
2023-09-02 10:18:13 +0100

    Add the notification model

---
+++ backend/rorapp/models/notification.py
@@ -0,0 +1,9 @@
+from django.db import models
+from rorapp.models.step import Step
+
+
+# Model for representing notifications
+class Notification(models.Model):
+    index = models.PositiveIntegerField()
+    step = models.ForeignKey(Step, on_delete=models.CASCADE)
+    type = models.CharField(max_length=50)
