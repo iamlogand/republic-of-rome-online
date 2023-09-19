iamlogand/republic-of-rome-online

commit 07a5d0c6ae529a9e7dea4b77d7f8bdcf41199dda

Delay import of `JwtAuthMiddleware` in ASGI config (#248)
In the ASGI configuration, delay import of `JwtAuthMiddleware` until after Django settings have been configured. This should fix a build failure in production that brought the API down a couple of days ago.

backend/rorsite/asgi.py
@@ -11,20 +11,21 @@ import os
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
-from rorapp.middleware.jwt_auth_middleware import JwtAuthMiddleware
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rorsite.settings')
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
-
 import rorapp.routing
 
 application = ProtocolTypeRouter(
     {
         "http": get_asgi_application(),
         "websocket": AllowedHostsOriginValidator(
-            JwtAuthMiddleware(URLRouter(rorapp.routing.websocket_urlpatterns))
+            # Import JwtAuthMiddleware here, after Django settings have been configured.
+            URLRouter(rorapp.routing.websocket_urlpatterns, middleware=[
+                'rorapp.middleware.jwt_auth_middleware.JwtAuthMiddleware'
+            ])
         ),
     }
 )

commit 5873f911e8a7b2a8feb42d7281c0aeabc267969f

Fix ASGI configuration
The ASGI configuration was still broken in commit 07a5d0c6ae529a9e7dea4b77d7f8bdcf41199dda because I was passing a `middleware` argument to `URLRouter` even though it doesn't accept such an argument. Fix this by importing the JWT auth middleware at the end of the file.

backend/rorsite/asgi.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 
 import os
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
+from channels.security.websocket import AllowedHostsOriginValidator, OriginValidator
 from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rorsite.settings')
 # Initialize Django ASGI application early to ensure the AppRegistry
@@ -22,10 +23,14 @@ application = ProtocolTypeRouter(
     {
         "http": get_asgi_application(),
         "websocket": AllowedHostsOriginValidator(
-            # Import JwtAuthMiddleware here, after Django settings have been configured.
-            URLRouter(rorapp.routing.websocket_urlpatterns, middleware=[
-                'rorapp.middleware.jwt_auth_middleware.JwtAuthMiddleware'
-            ])
+            AuthMiddlewareStack(
+                URLRouter(
+                    rorapp.routing.websocket_urlpatterns
+                )
+            )
         ),
     }
 )
+
+from rorapp.middleware import jwt_auth_middleware
+application = jwt_auth_middleware.JwtAuthMiddleware(application)