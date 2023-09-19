07a5d0c6ae529a9e7dea4b77d7f8bdcf41199dda
Logan Davidson <iamlogandavidson@gmail.com>
2023-09-19 08:58:05 +0100

    Delay import of `JwtAuthMiddleware` in ASGI config (#248)
    In the ASGI configuration, delay import of `JwtAuthMiddleware` until after Django settings have been configured. This should fix a build failure in production that brought the API down a couple of days ago.

--- backend/rorsite/asgi.py
+++ backend/rorsite/asgi.py
@@ -11,20 +11,21 @@ import os
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
-from rorapp.middleware.jwt_auth_middleware import JwtAuthMiddleware
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rorsite.settings')
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
-
 import rorapp.routing
 
 application = ProtocolTypeRouter(
     {
         "http": get_asgi_application(),
         "websocket": AllowedHostsOriginValidator(
-            JwtAuthMiddleware(URLRouter(rorapp.routing.websocket_urlpatterns))
+            # Import JwtAuthMiddleware here, after Django settings have been configured.
+            URLRouter(rorapp.routing.websocket_urlpatterns, middleware=[
+                'rorapp.middleware.jwt_auth_middleware.JwtAuthMiddleware'
+            ])
         ),
     }
 )
