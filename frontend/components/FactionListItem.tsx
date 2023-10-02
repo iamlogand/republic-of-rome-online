c9c5ffcc61f951eb2a64e0f4da24e1188cb32e2b
Logan Davidson <iamlogandavidson@gmail.com>
2023-10-02 20:41:31 +0100

    Add aggregate attributes to faction list items
    Add aggregate attributes to the factions in the faction list: senator count, total influence, total talents, total votes.

--- frontend/components/FactionListItem.tsx
+++ frontend/components/FactionListItem.tsx
@@ -1,5 +1,4 @@
-import { useEffect, useState } from 'react'
-import Stack from '@mui/material/Stack'
+import Image from 'next/image'
 
 import SenatorPortrait from '@/components/SenatorPortrait'
 import Collection from '@/classes/Collection'
@@ -8,6 +7,18 @@ import Senator from '@/classes/Senator'
 import styles from './FactionListItem.module.css'
 import { useGameContext } from '@/contexts/GameContext'
 import FactionLink from '@/components/FactionLink'
+import SenatorsIcon from "@/images/icons/senators.svg"
+import InfluenceIcon from "@/images/icons/influence.svg"
+import TalentsIcon from "@/images/icons/talents.svg"
+import VotesIcon from "@/images/icons/votes.svg"
+import Tooltip from '@mui/material/Tooltip'
+
+type Attribute = {
+  name: "senators" | "influence" | "votes" | "talents"
+  value: Number
+  image: string
+  sum?: boolean
+}
 
 interface FactionListItemProps {
   faction: Faction
@@ -22,6 +33,34 @@ const FactionListItem = (props: FactionListItemProps) => {
   const senators = new Collection<Senator>(
     allSenators.asArray.filter(s => s.faction === props.faction.id).sort((a, b) => a.name.localeCompare(b.name)) ?? []
   )
+  const senatorsCount = senators.allIds.length
+  const totalInfluence = senators.asArray.reduce((total, senator) => total + senator.influence, 0)
+  const totalTalents = senators.asArray.reduce((total, senator) => total + senator.talents, 0)
+  const totalVotes = senators.asArray.reduce((total, senator) => total + senator.votes, 0)
+
+  // Attribute data
+  const attributeItems: Attribute[] = [
+    { name: 'senators', value: senatorsCount, image: SenatorsIcon },
+    { name: 'influence', value: totalInfluence, image: InfluenceIcon, sum: true },
+    { name: 'talents', value: totalTalents, image: TalentsIcon, sum: true },
+    { name: 'votes', value: totalVotes, image: VotesIcon, sum: true }
+  ]
+
+  // Get attribute items
+  const getAttributeItem = (item: Attribute) => {
+    const titleCaseName = item.name[0].toUpperCase() + item.name.slice(1)
+    return (
+      <Tooltip title={(item.sum ? 'Total' : '') + ` ${titleCaseName}`} enterDelay={500} arrow>
+        <div key={item.name} className={`${styles.attribute} ` + (!item.sum ? `${styles.nonSum}` : '')}>
+          <div className={styles.symbols}>
+            {item.sum && <span className={styles.sigma}>Σ</span>}
+            <Image src={item.image} height={34} width={34} alt={`${titleCaseName} icon`} />
+          </div>
+          <div className={styles.attributeValue}>{item.value.toString()}</div>
+        </div>
+      </Tooltip>
+    )
+  }
 
   if (!player?.user || senators.allIds.length === 0) return null
 
@@ -30,7 +69,9 @@ const FactionListItem = (props: FactionListItemProps) => {
       <p>
         <b><FactionLink faction={props.faction} includeIcon /></b> of {player.user.username}
       </p>
-      <p>This faction has {senators.allIds.length} aligned senators</p>
+      <div className={styles.attributes}>
+        {attributeItems.map(item => getAttributeItem(item))}
+      </div>
       <div className={styles.portraits}>
         {senators.asArray.map((senator: Senator) =>
           <SenatorPortrait key={senator.id} senator={senator} size={80} selectable />
