faba847bb3c7d18051e7e0c2cc38c9137b2a87af
Logan Davidson <iamlogandavidson@gmail.com>
2023-10-04 20:21:38 +0100

    Add Tailwind CSS (#292)
    * Install Tailwind CSS and update Next
    
    * Configure Tailwind CSS
    
    * Use Tailwind to style the term detail sections
    
    * Downgrade Next
    
    Downgrade Next to resolve build warnings caused by a MUI/Next issue documented at https://github.com/vercel/next.js/issues/55663
    
    * Downgrade Next again
    
    Downgrade Next to 13.2.4 in an attempt to fix a build issue in the CI pipeline.
    
    * Fix the package-lock.json file

--- frontend/components/terms/Term_Hrao.tsx
+++ frontend/components/terms/Term_Hrao.tsx
@@ -1,20 +1,19 @@
 import Image from "next/image"
 
 import HRAOIcon from "@/images/icons/hrao.svg"
-import styles from "./Term.module.css"
 
 // Information about the game term: HRAO
 const HraoTerm = () => {
   return (
-    <div className={styles.termDetail}>
-      <div className={styles.iconAndTitle}>
+    <div className="px-4 py-2 flex flex-col gap-2">
+      <div className="flex items-center gap-2">
         <Image src={HRAOIcon} height={70} width={70} alt={`HRAO Icon`} />
         <h4>
           <b>HRAO</b> - <b>H</b>ighest <b>R</b>anking <b>A</b>vailable <b>O</b>
           fficial
         </h4>
       </div>
-      <div className={styles.textContainer}>
+      <div className="flex flex-col gap-2">
         <p>
           The HRAO is the highest ranking official in Rome. This senator is
           responsible for opening the Senate. The HRAO is typically the
