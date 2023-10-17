b9d877af165bf1989eb4407a827bb6dc0244cae4
Logan Davidson <iamlogandavidson@gmail.com>
2023-10-17 09:02:25 +0100

    Introduce a new color scheme
    Introduce a new color scheme. The colors can be viewed at `/colors`. The colors have been chosen but not used much in the site yet, except stone and white for backgrounds.

---
+++ frontend/pages/colors.tsx
@@ -0,0 +1,163 @@
+const ColorsPage = () => {
+  return (
+    <>
+      <div className="h-full w-full flex align-center text-black flex-wrap">
+        <div className="flex-1 flex flex-col items-stretch text-center">
+          <div className="bg-red-500 flex-1">
+            <p>Faction 1</p>
+            <p>RED</p>
+          </div>
+          <div className="bg-red-50 flex-1">50</div>
+          <div className="bg-red-100 flex-1">100</div>
+          <div className="bg-red-200 flex-1">200</div>
+          <div className="bg-red-300 flex-1">300</div>
+          <div className="bg-red-400 flex-1">400</div>
+          <div className="bg-red-500 flex-1">500</div>
+          <div className="bg-red-600 flex-1">600</div>
+          <div className="bg-red-700 flex-1">700</div>
+          <div className="bg-red-800 flex-1">800</div>
+          <div className="bg-red-900 flex-1">900</div>
+          <div className="bg-red-950 flex-1">950</div>
+        </div>
+        <div className="flex-1 flex flex-col items-stretch text-center">
+          <div className="bg-orange-500 flex-1">
+            <p>Faction 2</p>
+            <p>ORANGE</p>
+          </div>
+          <div className="bg-orange-50 flex-1">50</div>
+          <div className="bg-orange-100 flex-1">100</div>
+          <div className="bg-orange-200 flex-1">200</div>
+          <div className="bg-orange-300 flex-1">300</div>
+          <div className="bg-orange-400 flex-1">400</div>
+          <div className="bg-orange-500 flex-1">500</div>
+          <div className="bg-orange-600 flex-1">600</div>
+          <div className="bg-orange-700 flex-1">700</div>
+          <div className="bg-orange-800 flex-1">800</div>
+          <div className="bg-orange-900 flex-1">900</div>
+          <div className="bg-orange-950 flex-1">950</div>
+        </div>
+        <div className="flex-1 flex flex-col items-stretch text-center">
+          <div className="bg-yellow-500 flex-1">
+            <p>Faction 3</p>
+            <p>YELLOW</p>
+          </div>
+          <div className="bg-yellow-50 flex-1">50</div>
+          <div className="bg-yellow-100 flex-1">100</div>
+          <div className="bg-yellow-200 flex-1">200</div>
+          <div className="bg-yellow-300 flex-1">300</div>
+          <div className="bg-yellow-400 flex-1">400</div>
+          <div className="bg-yellow-500 flex-1">500</div>
+          <div className="bg-yellow-600 flex-1">600</div>
+          <div className="bg-yellow-700 flex-1">700</div>
+          <div className="bg-yellow-800 flex-1">800</div>
+          <div className="bg-yellow-900 flex-1">900</div>
+          <div className="bg-yellow-950 flex-1">950</div>
+        </div>
+        <div className="flex-1 flex flex-col items-stretch text-center">
+          <div className="bg-green-500 flex-1">
+            <p>Faction 4</p>
+            <p>GREEN</p>
+          </div>
+          <div className="bg-green-50 flex-1">50</div>
+          <div className="bg-green-100 flex-1">100</div>
+          <div className="bg-green-200 flex-1">200</div>
+          <div className="bg-green-300 flex-1">300</div>
+          <div className="bg-green-400 flex-1">400</div>
+          <div className="bg-green-500 flex-1">500</div>
+          <div className="bg-green-600 flex-1">600</div>
+          <div className="bg-green-700 flex-1">700</div>
+          <div className="bg-green-800 flex-1">800</div>
+          <div className="bg-green-900 flex-1">900</div>
+          <div className="bg-green-950 flex-1">950</div>
+        </div>
+        <div className="flex-1 flex flex-col items-stretch text-center">
+          <div className="bg-blue-500 flex-1">
+            <p>Faction 5</p>
+            <p>BLUE</p>
+          </div>
+          <div className="bg-blue-50 flex-1">50</div>
+          <div className="bg-blue-100 flex-1">100</div>
+          <div className="bg-blue-200 flex-1">200</div>
+          <div className="bg-blue-300 flex-1">300</div>
+          <div className="bg-blue-400 flex-1">400</div>
+          <div className="bg-blue-500 flex-1">500</div>
+          <div className="bg-blue-600 flex-1">600</div>
+          <div className="bg-blue-700 flex-1">700</div>
+          <div className="bg-blue-800 flex-1">800</div>
+          <div className="bg-blue-900 flex-1">900</div>
+          <div className="bg-blue-950 flex-1">950</div>
+        </div>
+        <div className="flex-1 flex flex-col items-stretch text-center">
+          <div className="bg-purple-500 flex-1">
+            <p>Faction 6</p>
+            <p>PURPLE</p>
+          </div>
+          <div className="bg-purple-50 flex-1">50</div>
+          <div className="bg-purple-100 flex-1">100</div>
+          <div className="bg-purple-200 flex-1">200</div>
+          <div className="bg-purple-300 flex-1">300</div>
+          <div className="bg-purple-400 flex-1">400</div>
+          <div className="bg-purple-500 flex-1">500</div>
+          <div className="bg-purple-600 flex-1">600</div>
+          <div className="bg-purple-700 flex-1">700</div>
+          <div className="bg-purple-800 flex-1">800</div>
+          <div className="bg-purple-900 flex-1">900</div>
+          <div className="bg-purple-950 flex-1">950</div>
+        </div>
+        <div className="flex-1 flex flex-col items-stretch text-center">
+          <div className="bg-tyrian-500 flex-1">
+            <p>Primary</p>
+            <p>TYRIAN</p>
+          </div>
+          <div className="bg-tyrian-50 flex-1">50</div>
+          <div className="bg-tyrian-100 flex-1">100</div>
+          <div className="bg-tyrian-200 flex-1">200</div>
+          <div className="bg-tyrian-300 flex-1">300</div>
+          <div className="bg-tyrian-400 flex-1">400</div>
+          <div className="bg-tyrian-500 flex-1">500</div>
+          <div className="bg-tyrian-600 flex-1">600</div>
+          <div className="bg-tyrian-700 flex-1">700</div>
+          <div className="bg-tyrian-800 flex-1">800</div>
+          <div className="bg-tyrian-900 flex-1">900</div>
+          <div className="bg-tyrian-950 flex-1">950</div>
+        </div>
+        <div className="flex-1 flex flex-col items-stretch text-center">
+          <div className="bg-teal-500 flex-1">
+            <p>Secondary</p>
+            <p>TEAL</p>
+          </div>
+          <div className="bg-teal-50 flex-1">50</div>
+          <div className="bg-teal-100 flex-1">100</div>
+          <div className="bg-teal-200 flex-1">200</div>
+          <div className="bg-teal-300 flex-1">300</div>
+          <div className="bg-teal-400 flex-1">400</div>
+          <div className="bg-teal-500 flex-1">500</div>
+          <div className="bg-teal-600 flex-1">600</div>
+          <div className="bg-teal-700 flex-1">700</div>
+          <div className="bg-teal-800 flex-1">800</div>
+          <div className="bg-teal-900 flex-1">900</div>
+          <div className="bg-teal-950 flex-1">950</div>
+        </div>
+        <div className="flex-1 flex flex-col items-stretch text-center">
+          <div className="bg-stone-500 flex-1">
+            <p>Neutral</p>
+            <p>STONE</p>
+          </div>
+          <div className="bg-stone-50 flex-1">50</div>
+          <div className="bg-stone-100 flex-1">100</div>
+          <div className="bg-stone-200 flex-1">200</div>
+          <div className="bg-stone-300 flex-1">300</div>
+          <div className="bg-stone-400 flex-1">400</div>
+          <div className="bg-stone-500 flex-1">500</div>
+          <div className="bg-stone-600 flex-1">600</div>
+          <div className="bg-stone-700 flex-1">700</div>
+          <div className="bg-stone-800 flex-1">800</div>
+          <div className="bg-stone-900 flex-1">900</div>
+          <div className="bg-stone-950 flex-1">950</div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default ColorsPage
