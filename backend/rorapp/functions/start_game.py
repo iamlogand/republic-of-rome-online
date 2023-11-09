6a4a5f61146147d2af1dc1b548abfe271eb7a4d2
Logan Davidson <iamlogandavidson@gmail.com>
2023-11-09 19:54:12 +0000

    Add anonymous start game function (#317)
    * Add anonymous start game function
    
    Refactor the start game function. Also format this file and some related files. There are now anonymous and user-specified versions of the start game function: `start_game` and `user_start_game`. The anonymous version may be useful for testing purposes.
    
    * Make a test use the anonymous start game function

--- backend/rorapp/functions/start_game.py
+++ backend/rorapp/functions/start_game.py
@@ -1,65 +1,106 @@
 import os
 import json
 import random
+from typing import Tuple
 from django.conf import settings
 from django.utils import timezone
 from rest_framework.response import Response
 from rest_framework.exceptions import NotFound, PermissionDenied
-from channels.layers import get_channel_layer
-from asgiref.sync import async_to_sync
 from rorapp.functions.rank_senators_and_factions import rank_senators_and_factions
 from rorapp.functions.send_websocket_messages import send_websocket_messages
 from rorapp.functions.ws_message_create import ws_message_create
 from rorapp.functions.ws_message_update import ws_message_update
-from rorapp.models import Game, Player, Faction, Senator, Title, Turn, Phase, Step, PotentialAction, ActionLog, SenatorActionLog
-from rorapp.serializers import GameDetailSerializer, TurnSerializer, PhaseSerializer, StepSerializer
+from rorapp.models import (
+    Game,
+    Player,
+    Faction,
+    Senator,
+    Title,
+    Turn,
+    Phase,
+    Step,
+    PotentialAction,
+    ActionLog,
+    SenatorActionLog,
+)
+from rorapp.serializers import (
+    GameDetailSerializer,
+    TurnSerializer,
+    PhaseSerializer,
+    StepSerializer,
+)
 
 
-def start_game(game_id, user, seed=None):
-    '''
-    Start and setup an early republic scenario game.
-    
+def user_start_game(game_id, user, seed=None) -> Response:
+    """
+    Start and setup an early republic scenario game as a user.
+
     :param game: the game id
     :param faction: the user starting the game
     :param seed: seed for controlling "random" operations when testing
-    
+
     :return: a response with a message and a status code
     :rtype: rest_framework.response.Response
-    '''
-    
+    """
     try:
-        game, players = validate_game_start(game_id, user)
+        validate_user(game_id, user)
+    except (NotFound, PermissionDenied) as e:
+        return Response({"message": str(e)}, status=e.status_code)
+
+    return start_game(game_id, seed)
+
+
+def start_game(game_id, seed=None) -> Response:
+    """
+    Start and setup an early republic scenario game anonymously.
+
+    :param game: the game id
+    :param seed: seed for controlling "random" operations when testing
+
+    :return: a response with a message and a status code
+    :rtype: rest_framework.response.Response
+    """
+
+    try:
+        game, players = validate_game_start(game_id)
         game, turn, phase, step = setup_game(game, players, seed)
         return send_start_game_websocket_messages(game, turn, phase, step)
     except (NotFound, PermissionDenied) as e:
         return Response({"message": str(e)}, status=e.status_code)
 
 
-def validate_game_start(game_id, user):
+def validate_user(game_id, user) -> None:
     try:
         game = Game.objects.get(id=game_id)
     except Game.DoesNotExist:
         raise NotFound("Game not found")
-    
+
     if game.host.id != user.id:
         raise PermissionDenied("Only the host can start the game")
-    
+
+
+def validate_game_start(game_id) -> Tuple[Game, list[Player]]:
+    try:
+        game = Game.objects.get(id=game_id)
+    except Game.DoesNotExist:
+        raise NotFound("Game not found")
+
     if Step.objects.filter(phase__turn__game__id=game.id).count() > 0:
         raise PermissionDenied("Game has already started")
-    
+
     players = Player.objects.filter(game__id=game.id)
     if players.count() < 3:
         raise PermissionDenied("Game must have at least 3 players to start")
-    
+
     return game, players
 
 
-def setup_game(game, players, seed):
+def setup_game(game, players, seed) -> Tuple[Game, Turn, Phase, Step]:
     factions = create_factions(game, players, seed)
     senators = create_senators(game, players, seed)
     assign_senators_to_factions(senators, factions)
     set_game_as_started(game)
-    
+
     turn, phase, step = create_turn_phase_step(game)
     temp_rome_consul_title = assign_temp_rome_consul(senators, step, seed)
 
@@ -70,15 +111,15 @@ def setup_game(game, players, seed):
     return game, turn, phase, step
 
 
-def create_factions(game, players, seed):
+def create_factions(game, players, seed) -> list[Faction]:
     factions = []
     position = 1
     random.seed() if seed is None else random.seed(seed)
     list_of_players = list(players)
     random.shuffle(list_of_players)
-    
+
     position_exclusions = [4, 6, 2]
-    positions_to_exclude = position_exclusions[:(6 - len(list_of_players))]
+    positions_to_exclude = position_exclusions[: (6 - len(list_of_players))]
 
     for player in list_of_players:
         while position in positions_to_exclude:
@@ -90,40 +131,42 @@ def create_factions(game, players, seed):
     return factions
 
 
-def create_senators(game, factions, seed):
+def create_senators(game, factions, seed) -> list[Senator]:
     candidate_senators = load_candidate_senators(game)
-    
+
     required_senator_count = len(factions) * 3
-    
+
     random.seed() if seed is None else random.seed(seed)
     random.shuffle(candidate_senators)
-    
+
     # Discard some candidates, leaving only the required number of senators
     return candidate_senators[:required_senator_count]
 
 
 def load_candidate_senators(game):
-    senator_json_path = os.path.join(settings.BASE_DIR, 'rorapp', 'presets', 'senator.json')
+    senator_json_path = os.path.join(
+        settings.BASE_DIR, "rorapp", "presets", "senator.json"
+    )
     senators = []
-    with open(senator_json_path, 'r') as file:
+    with open(senator_json_path, "r") as file:
         senators_dict = json.load(file)
-        
+
     for senator_name, senator_data in senators_dict.items():
-        if senator_data['scenario'] == 1:
+        if senator_data["scenario"] == 1:
             senator = Senator(
                 name=senator_name,
                 game=game,
-                code=senator_data['code'],
-                military=senator_data['military'],
-                oratory=senator_data['oratory'],
-                loyalty=senator_data['loyalty'],
-                influence=senator_data['influence']
+                code=senator_data["code"],
+                military=senator_data["military"],
+                oratory=senator_data["oratory"],
+                loyalty=senator_data["loyalty"],
+                influence=senator_data["influence"],
             )
             senators.append(senator)
     return senators
 
 
-def assign_senators_to_factions(senators, factions):
+def assign_senators_to_factions(senators, factions) -> None:
     senator_iterator = iter(senators)
     for faction in factions:
         for _ in range(3):
@@ -132,12 +175,12 @@ def assign_senators_to_factions(senators, factions):
             senator.save()  # Save senators to DB
 
 
-def set_game_as_started(game):
+def set_game_as_started(game) -> None:
     game.start_date = timezone.now()
     game.save()  # Update game to DB
 
 
-def create_turn_phase_step(game):
+def create_turn_phase_step(game) -> Tuple[Turn, Phase, Step]:
     turn = Turn(index=1, game=game)
     turn.save()
     phase = Phase(name="Faction", index=0, turn=turn)
@@ -147,45 +190,44 @@ def create_turn_phase_step(game):
     return turn, phase, step
 
 
-def assign_temp_rome_consul(senators, step, seed):
+def assign_temp_rome_consul(senators, step, seed) -> Title:
     random.seed() if seed is None else random.seed(seed)
     random.shuffle(senators)
-    
+
     temp_rome_consul_title = Title(
         name="Temporary Rome Consul",
         senator=senators[0],
         start_step=step,
-        major_office=True
+        major_office=True,
     )
     temp_rome_consul_title.save()
     return temp_rome_consul_title
 
 
-def create_action_logs(temp_rome_consul_title, step):
+def create_action_logs(temp_rome_consul_title, step) -> None:
     action_log = ActionLog(
         index=0,
         step=step,
         type="temporary_rome_consul",
         faction=temp_rome_consul_title.senator.faction,
-        data={"senator": temp_rome_consul_title.senator.id}
+        data={"senator": temp_rome_consul_title.senator.id},
     )
     action_log.save()
-    
+
     senator_action_log = SenatorActionLog(
-        senator=temp_rome_consul_title.senator,
-        action_log=action_log
+        senator=temp_rome_consul_title.senator, action_log=action_log
     )
     senator_action_log.save()
 
 
-def create_potential_actions(factions, step):
+def create_potential_actions(factions, step) -> None:
     for faction in factions:
         action = PotentialAction(
             step=step,
             faction=faction,
             type="select_faction_leader",
             required=True,
-            parameters=None
+            parameters=None,
         )
         action.save()
 
@@ -195,7 +237,7 @@ def send_start_game_websocket_messages(game, turn, phase, step):
         ws_message_update("game", GameDetailSerializer(game).data),
         ws_message_create("turn", TurnSerializer(turn).data),
         ws_message_create("phase", PhaseSerializer(phase).data),
-        ws_message_create("step", StepSerializer(step).data)
+        ws_message_create("step", StepSerializer(step).data),
     ]
     send_websocket_messages(game.id, messages_to_send)
     return Response({"message": "Game started"}, status=200)
--- backend/rorapp/views/start_game.py
+++ backend/rorapp/views/start_game.py
@@ -1,17 +1,16 @@
 from django.db import transaction
 from rest_framework import viewsets
 from rest_framework.decorators import action
-from rorapp.functions.start_game import start_game as start_game_direct
+from rorapp.functions.start_game import user_start_game
 
 
 class StartGameViewSet(viewsets.ViewSet):
-    '''
+    """
     Start and setup an early republic scenario game.
-    '''
-    
-    @action(detail=True, methods=['post'])
+    """
+
+    @action(detail=True, methods=["post"])
     @transaction.atomic
     def start_game(self, request, game_id=None):
-        
         # This viewset method wraps the start_game function in a transaction
-        return start_game_direct(game_id, request.user)
+        return user_start_game(game_id, request.user)
