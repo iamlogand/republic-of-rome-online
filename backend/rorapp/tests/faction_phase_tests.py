68ebf8c6e25f69bf6745f78199ba26d3f51e09a2
Logan Davidson <iamlogandavidson@gmail.com>
2023-11-17 15:35:59 +0000

    Refactor the faction phase test
    Refactor the faction phase test to improve readability by extracting logic into methods.

--- backend/rorapp/tests/faction_phase_tests.py
+++ backend/rorapp/tests/faction_phase_tests.py
@@ -3,88 +3,106 @@ from django.test import TestCase
 from rest_framework.test import APIClient
 from django.contrib.auth.models import User
 from rorapp.functions import delete_all_games, generate_game, start_game
-from rorapp.models import Action, Faction, Phase, Player, Senator, Title
+from rorapp.models import Action, Faction, Phase, Senator, Title
 
 
 class FactionPhaseTests(TestCase):
     """
     These tests check that players can select their first faction leader during the faction phase.
     """
-    
-    def setUp(self):
+
+    def setUp(self: TestCase) -> None:
         delete_all_games()
 
-    def test_faction_phase(self):
-        # Create 4 games with 3, 4, 5, and 6 players
-        game_ids = []
+    def test_faction_phase(self: TestCase) -> None:
         for player_count in range(3, 7):
-            game_id = generate_game(player_count)
-            start_game(game_id, seed=1)
-            game_ids.append(game_id)
-        self.game_ids: List[int] = game_ids
+            self.do_faction_phase_test(player_count)
 
-        for game_id in self.game_ids:
-            player_count = Player.objects.filter(game=game_id).count()
+    def do_faction_phase_test(self: TestCase, player_count: int) -> None:
+        game_id = self.setup_game_in_faction_phase(player_count)
+        self.check_latest_phase(game_id, 1, "Faction")
 
-            # Ensure phase is correct
-            phases = Phase.objects.filter(turn__game=game_id)
-            self.assertEqual(phases.count(), 1)
-            latest_phase = phases[0]
-            self.assertEqual(latest_phase.name, "Faction")
+        potential_actions_for_all_players = self.check_actions(
+            game_id, player_count, False
+        )
 
-            # Ensure potential actions are correct
-            potential_actions_for_all_players = Action.objects.filter(
-                step__phase__turn__game=game_id, completed=False
+        self.submit_actions(game_id, player_count, potential_actions_for_all_players)
+
+        self.check_actions(game_id, player_count, True)
+
+        self.check_faction_leader_titles(game_id, player_count)
+        self.check_latest_phase(game_id, 2, "Mortality")
+
+    def setup_game_in_faction_phase(self: TestCase, player_count: int) -> int:
+        game_id = generate_game(player_count)
+        start_game(game_id, seed=1)
+        self.assertEqual(Faction.objects.filter(game=game_id).count(), player_count)
+        return game_id
+
+    def check_latest_phase(
+        self: TestCase,
+        game_id: int,
+        expected_phase_count: int,
+        expected_latest_phase_name: str,
+    ) -> None:
+        phases = Phase.objects.filter(turn__game=game_id)
+        self.assertEqual(phases.count(), expected_phase_count)
+        latest_phase = phases[len(phases) - 1]
+        self.assertEqual(latest_phase.name, expected_latest_phase_name)
+
+    def submit_actions(
+        self: TestCase,
+        game_id: int,
+        player_count: int,
+        potential_actions_for_all_players: List[Action],
+    ) -> None:
+        for player_number in range(1, player_count + 1):
+            self.submit_action(
+                player_number, game_id, potential_actions_for_all_players
             )
-            self.assertEqual(potential_actions_for_all_players.count(), player_count)
-            faction_ids_with_correct_action = []
-            for action in potential_actions_for_all_players:
-                self.assertEqual(action.type, "select_faction_leader")
-                faction_ids_with_correct_action.append(action.faction.id)
-            self.assertEqual(len(set(faction_ids_with_correct_action)), player_count)
 
-            # Submit actions to select faction leaders
-            for player_number in range(1, player_count + 1):
-                user = User.objects.get(username=f"TestUser{player_number}")
-                user_faction_id = (
-                    Faction.objects.filter(player__user=user).get(game=game_id).id
-                )
-                user_potential_action_id = potential_actions_for_all_players.get(
-                    faction=user_faction_id
-                ).id
-                user_senators = Senator.objects.filter(
-                    faction=user_faction_id
-                ).order_by("name")
-                user_first_senator_id = user_senators[0].id
+    def submit_action(
+        self: TestCase,
+        player_number: int,
+        game_id: int,
+        potential_actions_for_all_players: List[Action],
+    ) -> None:
+        user = User.objects.get(username=f"TestUser{player_number}")
+        user_faction_id = Faction.objects.filter(player__user=user).get(game=game_id).id
+        user_potential_action_id = potential_actions_for_all_players.get(
+            faction=user_faction_id
+        ).id
+        user_senators = Senator.objects.filter(faction=user_faction_id).order_by("name")
+        user_first_senator_id = user_senators[0].id
 
-                self.client = APIClient()
-                self.client.force_authenticate(user=user)
-                response = self.client.post(
-                    f"/api/games/{game_id}/submit-action/{user_potential_action_id}/",
-                    data={"leader_id": user_first_senator_id},
-                )
-                self.assertEqual(response.status_code, 200)
+        self.client = APIClient()
+        self.client.force_authenticate(user=user)
+        response = self.client.post(
+            f"/api/games/{game_id}/submit-action/{user_potential_action_id}/",
+            data={"leader_id": user_first_senator_id},
+        )
+        self.assertEqual(response.status_code, 200)
 
-            # Ensure actions are completed
-            potential_actions_for_all_players = Action.objects.filter(
-                step__phase__turn__game=game_id,
-                completed=False,
-                type="select_faction_leader",
-            )
-            self.assertEqual(potential_actions_for_all_players.count(), 0)
-            completed_actions_for_all_players = Action.objects.filter(
-                step__phase__turn__game=game_id, completed=True
-            )
-            self.assertEqual(completed_actions_for_all_players.count(), player_count)
+    def check_actions(
+        self: TestCase, game_id: int, player_count: int, completed: bool
+    ) -> List[Action]:
+        potential_actions_for_all_players = Action.objects.filter(
+            step__phase__turn__game=game_id,
+            completed=completed,
+            type="select_faction_leader",
+        )
+        self.assertEqual(potential_actions_for_all_players.count(), player_count)
+        faction_ids_with_correct_action = []
+        for action in potential_actions_for_all_players:
+            self.assertEqual(action.type, "select_faction_leader")
+            faction_ids_with_correct_action.append(action.faction.id)
+        self.assertEqual(len(set(faction_ids_with_correct_action)), player_count)
+        return potential_actions_for_all_players
 
-            # Ensure faction leader titles have been assigned
-            titles = Title.objects.filter(
-                senator__faction__game=game_id, name="Faction Leader"
-            )
-            self.assertEqual(titles.count(), player_count)
-
-            # Ensure phase is mortality
-            phases = Phase.objects.filter(turn__game=game_id).order_by("-index")
-            self.assertEqual(phases.count(), 2)
-            latest_phase = phases[0]
-            self.assertEqual(latest_phase.name, "Mortality")
+    def check_faction_leader_titles(
+        self: TestCase, game_id: int, player_count: int
+    ) -> None:
+        titles = Title.objects.filter(
+            senator__faction__game=game_id, name="Faction Leader"
+        )
+        self.assertEqual(titles.count(), player_count)
