d7783fcb1de860fd151ccc23365e0909cd07ed6c
Logan Davidson <iamlogandavidson@gmail.com>
2023-12-02 13:36:37 +0000

    Improve the Senator admin page
    Improve the Senator admin page to improve developer experience.

--- backend/rorapp/admin/senator.py
+++ backend/rorapp/admin/senator.py
@@ -5,4 +5,22 @@ from rorapp.models import Senator
 # Admin configuration for senators
 @admin.register(Senator)
 class SenatorAdmin(admin.ModelAdmin):
-    list_display = ("__str__", 'name', 'game', 'faction', 'death_step', 'code', 'generation', 'rank')
+    list_display = (
+        "__str__",
+        "game",
+        "faction",
+        "name",
+        "code",
+        "generation",
+        "death_step",
+        "rank",
+    )
+    search_fields = (
+        "game__id",
+        "faction__id",
+        "name",
+        "code",
+        "generation",
+        "death_step__id",
+        "rank",
+    )
