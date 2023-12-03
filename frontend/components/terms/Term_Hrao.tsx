3e7f5768f78d94279809814d1ebf796284747fc8
Logan Davidson <iamlogandavidson@gmail.com>
2023-12-03 10:29:14 +0000

    Improve detail sections slightly (#353)
    Slightly improve the term details and the faction description.

--- frontend/components/terms/Term_Hrao.tsx
+++ frontend/components/terms/Term_Hrao.tsx
@@ -2,19 +2,27 @@ import Image from "next/image"
 
 import HRAOIcon from "@/images/icons/hrao.svg"
 import TermLink from "@/components/TermLink"
+import { Avatar } from "@mui/material"
 
 // Information about the game term: HRAO
 const HraoTerm = () => {
   return (
-    <div className="px-4 py-2 flex flex-col gap-2">
-      <div className="flex items-center gap-2">
-        <Image src={HRAOIcon} height={70} width={70} alt={`HRAO Icon`} />
+    <div className="p-6 flex flex-col gap-4">
+      <div className="flex items-center gap-4">
+        <Avatar className="h-14 w-14 bg-tyrian-100">
+          <Image
+            src={HRAOIcon}
+            height={40}
+            width={40}
+            alt={`HRAO Icon`}
+          />
+        </Avatar>
         <h4>
           <b>HRAO</b> - <b>H</b>ighest <b>R</b>anking <b>A</b>vailable <b>O</b>
           fficial
         </h4>
       </div>
-      <div className="flex flex-col gap-2">
+      <div className="flex flex-col gap-4">
         <p>
           The HRAO is the highest ranking official in Rome. This senator is
           responsible for opening the Senate. The HRAO is typically the
@@ -24,7 +32,9 @@ const HraoTerm = () => {
         <p>The order of precedence for determining the HRAO is:</p>
         <ol>
           <li>Dictator</li>
-          <li><TermLink name="Rome Consul" includeIcon /></li>
+          <li>
+            <TermLink name="Rome Consul" includeIcon />
+          </li>
           <li>Field Consul</li>
           <li>Censor</li>
           <li>Master of Horse</li>
