e5a7c530de99becf1a11522779c04a01c0a0e976
Logan Davidson <iamlogandavidson@gmail.com>
2024-01-18 07:48:29 +0000

    Add a meta button that links to the secrets list (#379)
    * Add a meta button that links to the secrets list
    
    Allow the `AttributeFlex` component to accept `Attribute` items with `onClick` values so that specific attributes can behave as buttons. Use this to make the Secrets attribute in the meta section behave as a link that opens the list of secrets.
    
    * Add missing `useCallback` dependency array items

--- frontend/components/AttributeFlex.tsx
+++ frontend/components/AttributeFlex.tsx
@@ -6,37 +6,50 @@ export interface Attribute {
   name: string
   value: number
   icon: string
+  onClick?: () => void
 }
 
 interface AttributeGridProps {
   attributes: Attribute[]
 }
 
-const AttributeFlex = ({ attributes }: AttributeGridProps) => {
-  // Get attribute items
-  const getAttributeItem = (item: Attribute, index: number) => {
-    const titleCaseName = capitalize(item.name)
-    return (
-      <Tooltip key={index} title={titleCaseName} enterDelay={500} arrow>
-        <div className="w-[64px] grid grid-cols-[30px_30px] items-center justify-center bg-white shadow-[0px_0px_2px_2px_white] rounded">
-          <Image
-            src={item.icon}
-            height={28}
-            width={28}
-            alt={`${titleCaseName} icon`}
-          />
-          <div className="w-8 text-center text-md font-semibold">
-            {item.value.toString()}
-          </div>
+const getAttributeItem = (item: Attribute, index?: number) => {
+  const titleCaseName = capitalize(item.name)
+  return (
+    <Tooltip key={index} title={titleCaseName} enterDelay={500} arrow>
+      <div className="w-[64px] grid grid-cols-[30px_30px] items-center justify-center bg-white shadow-[0px_0px_2px_2px_white] rounded">
+        <Image
+          src={item.icon}
+          height={28}
+          width={28}
+          alt={`${titleCaseName} icon`}
+        />
+        <div className="w-8 text-center text-md font-semibold">
+          {item.value.toString()}
         </div>
-      </Tooltip>
+      </div>
+    </Tooltip>
+  )
+}
+
+const AttributeFlex = ({ attributes }: AttributeGridProps) => {
+  const getButton = (item: Attribute, index: number) => {
+    if (item.onClick === undefined) return getAttributeItem(item, index)
+    return (
+      <button
+        key={index}
+        onClick={item.onClick}
+        className="cursor-pointer border-0 p-0 bg-transparent"
+      >
+        {getAttributeItem(item)}
+      </button>
     )
   }
 
   return (
     <div className="p-[2px] flex flex-wrap gap-3 select-none">
       {attributes.map((attribute: Attribute, index) =>
-        getAttributeItem(attribute, index)
+        getButton(attribute, index)
       )}
     </div>
   )
