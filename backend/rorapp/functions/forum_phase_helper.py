iamlogand/republic-of-rome-online

commit 07bf5af6367ae68f9f386863e3109b3c2cf1eb07

Add progress helper functions (#404)
* Add progress helper functions

Add progress helper functions and use one of them, the `get_latest_step` function, in some places.

* Use the `get_latest_step` function

Use the `get_latest_step` function in the remaining places where it can be used.

* Remove the `get_latest_step` function

Remove the `get_latest_step` function and use the `get_latest_phase` function.

backend/rorapp/functions/forum_phase_helper.py
@@ -1,5 +1,6 @@
 from rest_framework.response import Response
 from typing import List, Optional
+from rorapp.functions.progress_helper import get_latest_phase, get_latest_step
 from rorapp.functions.senator_helper import create_new_family
 from rorapp.functions.war_helper import create_new_war
 from rorapp.functions.enemy_leader_helper import create_new_enemy_leader
@@ -60,13 +61,9 @@ def generate_initiate_situation_action(faction: Faction) -> List[dict]:
     messages_to_send = []
 
     # Create new step
-    latest_step = (
-        Step.objects.filter(phase__turn__game=faction.game.id).order_by("index").last()
-    )
+    latest_step = get_latest_step(faction.game.id)
     # Need to get latest phase because the latest step might not be from the current forum phase
-    latest_phase = (
-        Phase.objects.filter(turn=latest_step.phase.turn).order_by("index").last()
-    )
+    latest_phase = get_latest_phase(faction.game.id)
     new_step = Step(index=latest_step.index + 1, phase=latest_phase)
     new_step.save()
     messages_to_send.append(

backend/rorapp/functions/progress_helper.py
@@ -1,0 +1,19 @@
+from rorapp.models import Phase, Step
+
+
+def get_latest_step(game_id: int, reverse_index: int = 0) -> Step:
+    if reverse_index == 0:
+        return Step.objects.filter(phase__turn__game=game_id).order_by("-index").first()
+    else:
+        return Step.objects.filter(phase__turn__game=game_id).order_by("-index")[
+            reverse_index
+        ]
+
+
+def get_latest_phase(game_id: int, reverse_index: int = 0) -> Phase:
+    if reverse_index == 0:
+        return Phase.objects.filter(turn__game=game_id).order_by("-index").first()
+    else:
+        return Phase.objects.filter(turn__game=game_id).order_by("-index")[
+            reverse_index
+        ]

backend/rorapp/functions/__init__.py
@@ -1,16 +1,20 @@
 # The functions module provides a collection of public functions that are intended to be used by other parts of the application.
-from .faction_leader_helper import select_faction_leader_from_action, select_faction_leader  # noqa: F401
+from .enemy_leader_helper import create_new_enemy_leader  # noqa: F401
+from .faction_leader_helper import (
+    select_faction_leader_from_action,
+    select_faction_leader,
+)  # noqa: F401
 from .forum_phase_helper import initiate_situation  # noqa: F401
 from .forum_phase_starter import start_forum_phase  # noqa: F401
 from .game_deleter import delete_all_games  # noqa: F401
 from .game_generator import generate_game  # noqa: F401
 from .game_starter import start_game, user_start_game  # noqa: F401
-from .enemy_leader_helper import create_new_enemy_leader  # noqa: F401
 from .mortality_phase_helper import (
     face_mortality,  # noqa: F401
     resolve_mortality,  # noqa: F401
 )
 from .mortality_phase_starter import setup_mortality_phase  # noqa: F401
+from .progress_helper import get_latest_phase, get_latest_step  # noqa: F401
 from .user_helper import find_or_create_test_user  # noqa: F401
 from .war_helper import create_new_war  # noqa: F401
 from .websocket_message_helper import (

backend/rorapp/functions/action_helper.py
@@ -1,12 +1,11 @@
 from typing import List
+from rorapp.functions.progress_helper import get_latest_step
 from rorapp.functions.websocket_message_helper import destroy_websocket_message
-from rorapp.models import Action, Step
+from rorapp.models import Action
 
 
 def delete_old_actions(game_id: int) -> List[dict]:
-    latest_step = (
-        Step.objects.filter(phase__turn__game=game_id).order_by("-index").first()
-    )
+    latest_step = get_latest_step(game_id)
     actions = Action.objects.filter(
         step__phase__turn__game=game_id, step__index__lt=latest_step.index
     )

backend/rorapp/functions/enemy_leader_helper.py
@@ -2,8 +2,9 @@ import os
 import json
 from typing import List, Tuple
 from django.conf import settings
+from rorapp.functions.progress_helper import get_latest_step
 from rorapp.functions.websocket_message_helper import create_websocket_message
-from rorapp.models import ActionLog, EnemyLeader, Faction, Game, Step, War
+from rorapp.models import ActionLog, EnemyLeader, Faction, Game, War
 from rorapp.serializers import ActionLogSerializer, EnemyLeaderSerializer, WarSerializer
 
 
@@ -66,9 +67,7 @@ def create_new_enemy_leader(initiating_faction_id: int, name: str) -> List[dict]
         .index
         + 1
     )
-    latest_step = (
-        Step.objects.filter(phase__turn__game=game_id).order_by("-index").first()
-    )
+    latest_step = get_latest_step(game_id)
     action_log_data = {
         "enemy_leader": enemy_leader.id,
         "matching_war": matching_war.id if matching_war else None,

backend/rorapp/functions/faction_leader_helper.py
@@ -6,6 +6,7 @@ from rorapp.functions.forum_phase_helper import (
     get_next_faction_in_forum_phase,
 )
 from rorapp.functions.mortality_phase_starter import setup_mortality_phase
+from rorapp.functions.progress_helper import get_latest_step
 from rorapp.functions.websocket_message_helper import (
     create_websocket_message,
     destroy_websocket_message,
@@ -61,7 +62,7 @@ def select_faction_leader(senator_id: int) -> Tuple[Response, dict]:
     senator = Senator.objects.get(id=senator_id)
     game = Game.objects.get(id=senator.game.id)
     faction = Faction.objects.get(id=senator.faction.id)
-    step = Step.objects.filter(phase__turn__game=game.id).latest("index")
+    step = get_latest_step(game.id)
     action = Action.objects.get(
         step=step, faction=faction, type="select_faction_leader"
     )

backend/rorapp/functions/forum_phase_starter.py
@@ -1,12 +1,9 @@
 from typing import List
 from rorapp.functions.action_helper import delete_old_actions
 from rorapp.functions.forum_phase_helper import generate_initiate_situation_action
+from rorapp.functions.progress_helper import get_latest_step
 from rorapp.functions.websocket_message_helper import create_websocket_message
-from rorapp.models import (
-    Faction,
-    Phase,
-    Step,
-)
+from rorapp.models import Faction, Phase
 from rorapp.serializers import PhaseSerializer
 
 
@@ -21,7 +18,7 @@ def start_forum_phase(game_id: int) -> List[dict]:
         List[dict]: The WebSocket messages to send.
     """
     messages_to_send = []
-    latest_step = Step.objects.filter(phase__turn__game=game_id).order_by("-index")[0]
+    latest_step = get_latest_step(game_id)
 
     # Progress to the forum phase
     new_phase = Phase(

backend/rorapp/functions/mortality_phase_helper.py
@@ -5,6 +5,7 @@ from django.conf import settings
 from rest_framework.response import Response
 from rorapp.functions.forum_phase_starter import start_forum_phase
 from rorapp.functions.mortality_chit_helper import draw_mortality_chits
+from rorapp.functions.progress_helper import get_latest_step
 from rorapp.functions.rank_helper import rank_senators_and_factions
 from rorapp.functions.websocket_message_helper import (
     create_websocket_message,
@@ -16,7 +17,6 @@ from rorapp.models import (
     Game,
     Senator,
     SenatorActionLog,
-    Step,
     Title,
 )
 from rorapp.serializers import (
@@ -72,7 +72,7 @@ def resolve_mortality(game_id: int, chit_codes: List[int] | None = None) -> dict
     """
 
     game = Game.objects.get(id=game_id)
-    latest_step = Step.objects.filter(phase__turn__game=game_id).order_by("-index")[0]
+    latest_step = get_latest_step(game_id)
     # Read senator presets
     senator_json_path = os.path.join(
         settings.BASE_DIR, "rorapp", "presets", "senator.json"

commit 3f50136ba6a5e323855551c28deec6b5720a8b82

Fix the `get_latest_phase` function
Fix a bug where the `get_latest_phase` function was pretty much broken, causing the game to never progress beyond the second turn.

This was a regression introduced in #404.

backend/rorapp/functions/forum_phase_helper.py
@@ -12,7 +12,6 @@ from rorapp.functions.websocket_message_helper import (
 from rorapp.models import (
     Action,
     Faction,
-    Phase,
     Senator,
     Situation,
     Step,

backend/rorapp/functions/progress_helper.py
@@ -11,9 +11,10 @@ def get_latest_step(game_id: int, reverse_index: int = 0) -> Step:
 
 
 def get_latest_phase(game_id: int, reverse_index: int = 0) -> Phase:
+    latest_step = get_latest_step(game_id)
     if reverse_index == 0:
-        return Phase.objects.filter(turn__game=game_id).order_by("-index").first()
+        return Phase.objects.filter(turn=latest_step.phase.turn).order_by("-index").first()
     else:
-        return Phase.objects.filter(turn__game=game_id).order_by("-index")[
+        return Phase.objects.filter(turn=latest_step.phase.turn).order_by("-index")[
             reverse_index
         ]