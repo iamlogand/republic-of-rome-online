da0c07cadc3fefe57bd8fceee5e56dcf0ec04571
Logan Davidson <iamlogandavidson@gmail.com>
2024-03-08 23:06:28 +0000

    Add elapsed datetime
    Add elapsed datetime to the action logs and move the regular datetime to a ToolTip that only shows on hover.

--- frontend/components/ActionLogLayout.tsx
+++ frontend/components/ActionLogLayout.tsx
@@ -1,9 +1,9 @@
-import { CSSProperties, ReactNode } from "react"
-import { Alert } from "@mui/material"
+import { CSSProperties, ReactNode, useEffect, useState } from "react"
+import { Alert, Tooltip } from "@mui/material"
 import Cookies from "js-cookie"
 import Faction from "@/classes/Faction"
 import { useCookieContext } from "@/contexts/CookieContext"
-import formatDate from "@/functions/date"
+import formatDate, { formatElapsedDate } from "@/functions/date"
 import ActionLog from "@/classes/ActionLog"
 
 interface ActionLogLayoutProps {
@@ -24,6 +24,18 @@ const ActionLogLayout = ({
   const { darkMode } = useCookieContext()
   const timezone = Cookies.get("timezone")
 
+  // update the state to force rendering every 5 seconds
+  const [_, setRefreshKey] = useState(0)
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setRefreshKey((oldKey) => oldKey + 1)
+    }, 5000)
+
+    return () => {
+      clearInterval(interval) // cleanup on unmount
+    }
+  }, [])
+
   const getStyle = () => {
     let style: CSSProperties = { position: "relative" }
     if (faction) {
@@ -40,8 +52,10 @@ const ActionLogLayout = ({
   return (
     <Alert icon={icon} style={getStyle()}>
       {timezone && (
-        <div className="absolute right-1 top-0.5 text-xs opacity-50">
-          {formatDate(actionLog.creation_date, timezone)}
+        <div className="absolute right-2 top-0.5 text-xs opacity-90 cursor-default">
+          <Tooltip title={formatDate(actionLog.creation_date, timezone)} arrow>
+            <span>{formatElapsedDate(actionLog.creation_date, timezone)}</span>
+          </Tooltip>
         </div>
       )}
       <b>{title}</b>
