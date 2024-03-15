a76576e4ec5965a6f2efd1c296f945da1abd14b6
Logan Davidson <iamlogandavidson@gmail.com>
2024-03-15 16:22:21 +0000

    Reduce brightness of the unaligned sector

--- frontend/components/SenateSector.tsx
+++ frontend/components/SenateSector.tsx
@@ -99,7 +99,7 @@ const SenateSector = ({
           height: outerRadius * 2,
           backgroundColor:
             (darkMode ? faction?.getColor(700) : faction?.getColor(300)) ??
-            (darkMode ? "var(--neutral-550)" : "var(--neutral-200)"),
+            (darkMode ? "var(--neutral-600)" : "var(--neutral-200)"),
           clipPath: `path('\
             M ${outerRadius + getXOffset(startAngle, innerRadius)} ${
             outerRadius + getYOffset(startAngle, innerRadius)
