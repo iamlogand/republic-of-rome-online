1a38a127cacc9a630b3bc7fc1ab943431fe9901e
Logan Davidson <iamlogandavidson@gmail.com>
2024-03-16 17:28:06 +0000

    Add singular units of time (#444)
    * Add singular units of time
    
    * Simplify the change and fix typing regression

--- frontend/functions/date.ts
+++ frontend/functions/date.ts
@@ -24,31 +24,31 @@ export const formatElapsedDate = (date: Date, timezone: string) => {
 
   const minutes = Math.floor(seconds / 60)
   if (minutes < 60) {
-    return `${minutes} mins ago`
+    return `${minutes} ${minutes === 1 ? "min" : "mins"} ago`
   }
 
   const hours = Math.floor(minutes / 60)
   if (hours < 24) {
-    return `${hours} hours ago`
+    return `${hours} ${hours === 1 ? "hour" : "hours"} ago`
   }
 
   const days = Math.floor(hours / 24)
   if (days < 7) {
-    return `${days} days ago`
+    return `${days} ${days === 1 ? "day" : "days"} ago`
   }
 
   const weeks = Math.floor(days / 7)
   if (weeks < 4) {
-    return `${weeks} weeks ago`
+    return `${weeks} ${weeks === 1 ? "week" : "weeks"} ago`
   }
 
   const months = Math.floor(days / 30)
   if (months < 12) {
-    return `${months} months ago`
+    return `${months} ${months === 1 ? "month" : "months"} ago`
   }
 
   const years = Math.floor(days / 365)
-  return `${years} years ago`
+  return `${years} ${years === 1 ? "year" : "years"} ago`
 }
 
 export default formatDate
