27e34d1001eec27b86de7d686aa290fc067ad787
Logan Davidson <iamlogandavidson@gmail.com>
2024-03-16 17:21:13 +0000

    Add singular units of time

--- frontend/functions/date.ts
+++ frontend/functions/date.ts
@@ -1,6 +1,6 @@
 import { utcToZonedTime } from "date-fns-tz"
 
-const formatDate = (date: Date, timezone: string) => {
+const formatDate = (date, timezone) => {
   const zonedDate = utcToZonedTime(date, timezone)
 
   const year = zonedDate.getFullYear()
@@ -12,7 +12,7 @@ const formatDate = (date: Date, timezone: string) => {
   return `${year}-${month}-${day} ${hour}:${minute}`
 }
 
-export const formatElapsedDate = (date: Date, timezone: string) => {
+export const formatElapsedDate = (date, timezone) => {
   const zonedDate = utcToZonedTime(date, timezone)
   const now = new Date()
   const elapsed = now.getTime() - zonedDate.getTime()
@@ -24,31 +24,55 @@ export const formatElapsedDate = (date: Date, timezone: string) => {
 
   const minutes = Math.floor(seconds / 60)
   if (minutes < 60) {
-    return `${minutes} mins ago`
+    if (minutes === 1) {
+      return `1 min ago`
+    } else {
+      return `${minutes} mins ago`
+    }
   }
 
   const hours = Math.floor(minutes / 60)
   if (hours < 24) {
-    return `${hours} hours ago`
+    if (hours === 1) {
+      return `1 hour ago`
+    } else {
+      return `${hours} hours ago`
+    }
   }
 
   const days = Math.floor(hours / 24)
   if (days < 7) {
-    return `${days} days ago`
+    if (days == 1) {
+      return `1 day ago`
+    } else {
+      return `${days} days ago`
+    }
   }
 
   const weeks = Math.floor(days / 7)
   if (weeks < 4) {
-    return `${weeks} weeks ago`
+    if (weeks === 1) {
+      return `1 week ago`
+    } else {
+      return `${weeks} weeks ago`
+    }
   }
 
   const months = Math.floor(days / 30)
   if (months < 12) {
-    return `${months} months ago`
+    if (months === 1) {
+      return `1 month ago`
+    } else {
+      return `${months} months ago`
+    }
   }
 
   const years = Math.floor(days / 365)
-  return `${years} years ago`
+  if (years === 1) {
+    return `1 year ago`
+  } else {
+    return `${years} years ago`
+  }
 }
 
 export default formatDate
