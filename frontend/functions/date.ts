733690f193781e258f87e0de85b69a260c1fd9ed
Logan Davidson <iamlogandavidson@gmail.com>
2024-03-16 17:25:58 +0000

    Simplify the change and fix typing regression

--- frontend/functions/date.ts
+++ frontend/functions/date.ts
@@ -1,6 +1,6 @@
 import { utcToZonedTime } from "date-fns-tz"
 
-const formatDate = (date, timezone) => {
+const formatDate = (date: Date, timezone: string) => {
   const zonedDate = utcToZonedTime(date, timezone)
 
   const year = zonedDate.getFullYear()
@@ -12,7 +12,7 @@ const formatDate = (date, timezone) => {
   return `${year}-${month}-${day} ${hour}:${minute}`
 }
 
-export const formatElapsedDate = (date, timezone) => {
+export const formatElapsedDate = (date: Date, timezone: string) => {
   const zonedDate = utcToZonedTime(date, timezone)
   const now = new Date()
   const elapsed = now.getTime() - zonedDate.getTime()
@@ -24,55 +24,31 @@ export const formatElapsedDate = (date, timezone) => {
 
   const minutes = Math.floor(seconds / 60)
   if (minutes < 60) {
-    if (minutes === 1) {
-      return `1 min ago`
-    } else {
-      return `${minutes} mins ago`
-    }
+    return `${minutes} ${minutes === 1 ? "min" : "mins"} ago`
   }
 
   const hours = Math.floor(minutes / 60)
   if (hours < 24) {
-    if (hours === 1) {
-      return `1 hour ago`
-    } else {
-      return `${hours} hours ago`
-    }
+    return `${hours} ${hours === 1 ? "hour" : "hours"} ago`
   }
 
   const days = Math.floor(hours / 24)
   if (days < 7) {
-    if (days == 1) {
-      return `1 day ago`
-    } else {
-      return `${days} days ago`
-    }
+    return `${days} ${days === 1 ? "day" : "days"} ago`
   }
 
   const weeks = Math.floor(days / 7)
   if (weeks < 4) {
-    if (weeks === 1) {
-      return `1 week ago`
-    } else {
-      return `${weeks} weeks ago`
-    }
+    return `${weeks} ${weeks === 1 ? "week" : "weeks"} ago`
   }
 
   const months = Math.floor(days / 30)
   if (months < 12) {
-    if (months === 1) {
-      return `1 month ago`
-    } else {
-      return `${months} months ago`
-    }
+    return `${months} ${months === 1 ? "month" : "months"} ago`
   }
 
   const years = Math.floor(days / 365)
-  if (years === 1) {
-    return `1 year ago`
-  } else {
-    return `${years} years ago`
-  }
+  return `${years} ${years === 1 ? "year" : "years"} ago`
 }
 
 export default formatDate
