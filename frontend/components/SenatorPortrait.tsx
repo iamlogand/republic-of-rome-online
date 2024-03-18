iamlogand/republic-of-rome-online

commit 91080b358016893cb4a94a5fd7b2eb61926102d7

Improve the senator summary (#443)
* Make some senator summary improvements

* Add a delay to the senator summary popover

Add a delay to the senator summary popover open and tweak some other related stuff, including the senator portrait.

* Remove the last modular CSS

Remove the last modular CSS from the play page and replace with Tailwind or inline CSS.

* Fix senator portrait summary margin issue

Fix an issue where there was a margin at the bottom of senator portraits that have a senator summary.

* Increase summary margin

frontend/components/SenatorPortrait.tsx
@@ -1,10 +1,9 @@
-import { useState, MouseEvent } from "react"
+import { useState } from "react"
 import Image, { StaticImageData } from "next/image"
 import { Tooltip } from "@mui/material"
 
 import Senator from "@/classes/Senator"
 import Faction from "@/classes/Faction"
-import styles from "@/components/SenatorPortrait.module.css"
 import Title from "@/classes/Title"
 import TitleIcon from "@/components/TitleIcon"
 import SelectedDetail from "@/types/SelectedDetail"
@@ -166,7 +165,7 @@ const SenatorPortrait = ({
       background: "radial-gradient(" + innerBgColor + ", " + outerBgColor + ")",
       height: size - 6,
       width: size - 6,
-      borderRadius: round ? "50%" : "4px",
+      borderRadius: round ? "50%" : 4,
     }
   }
 
@@ -215,13 +214,15 @@ const SenatorPortrait = ({
       setSelectedDetail({ type: "Senator", id: senator.id } as SelectedDetail)
   }
 
+  const showIcons = size > 70
+
   // Get JSX for the portrait
   const PortraitElement = selectable ? "button" : "div"
 
   const getPortrait = () => (
     <PortraitElement
-      className={`select-none ${styles.senatorPortrait} ${
-        selectable ? styles.selectable : ""
+      className={`select-none ${
+        selectable ? "border-none p-0 bg-transparent" : ""
       }`}
       onMouseOver={handleMouseOver}
       onMouseLeave={handleMouseLeave}
@@ -231,31 +232,32 @@ const SenatorPortrait = ({
         style={{
           height: size,
           width: size,
-          borderRadius: round ? "50%" : "4px",
+          borderRadius: round ? "50%" : 4,
+          boxShadow: "0 0 4px 0 rgb(255 255 255 / 0.3)",
         }}
-        className="shadow bg-neutral-700 dark:bg-black"
+        className="relative flex justify-center items-center box-border bg-neutral-700 dark:bg-black"
       >
         <div
-          className={`${styles.imageContainer}`}
+          className="absolute z-10 overflow-hidden left-1/2 top-1/2 translate-x-[-50%] translate-y-[-50%]"
           style={getImageContainerStyle()}
         >
           {selectable &&
             selectedDetail?.type === "Senator" &&
             selectedDetail?.id === senator.id && (
               <div
-                className={`absolute w-full h-full z-[2] shadow-[inset_0_0_6px_4px_white]`}
+                className={`absolute w-full h-full z-30 shadow-[inset_0_0_6px_4px_white]`}
                 style={{ borderRadius: round ? "50%" : 0 }}
               ></div>
             )}
           {factionLeader && (
             <Image
               src={FactionLeaderPattern}
-              className={styles.factionLeaderPattern}
+              className="absolute h-auto w-full top-0 left-0"
               alt="Faction Leader pattern"
             />
           )}
           <Image
-            className={`${styles.picture} ${
+            className={`absolute z-10 left-1/2 top-1/2 ${
               senator.alive ? "" : "grayscale-[80%]"
             }`}
             width={size + getZoom()}
@@ -268,7 +270,7 @@ const SenatorPortrait = ({
           />
         </div>
         <div className="absolute" style={getBgStyle()}></div>
-        {majorOffice && (
+        {majorOffice && showIcons && (
           <TitleIcon
             title={majorOffice}
             size={getIconSize()}
@@ -280,20 +282,22 @@ const SenatorPortrait = ({
           <>
             {size > 120 && (
               <Tooltip title="Senator ID" arrow>
-                <div className={styles.code}># {senator.code}</div>
+                <div className="absolute z-60 bottom-1 px-[6px] py-0.5 text-sm text-white bg-[rgba(0,_0,_0,_0.4)] user-none">
+                  # {senator.code}
+                </div>
               </Tooltip>
             )}
-            {senator.alive === false && (
+            {senator.alive === false && showIcons && (
               <Image
                 src={DeadIcon}
                 alt="Skull and crossbones icon"
                 height={getIconSize()}
                 width={getIconSize()}
-                className={styles.deadIcon}
+                className="absolute right-[3px] bottom-[3px] z-20 box-border"
               />
             )}
             {debugShowEntityIds && (
-              <div className="z-[1000] absolute top-1 px-1 text-lg text-white bg-black/60 ">
+              <div className="z-100 absolute top-1 px-1 text-lg text-white bg-black/60 ">
                 {senator.id}
               </div>
             )}

frontend/components/EnemyLeaderPortrait.tsx
@@ -61,7 +61,11 @@ const EnemyLeaderPortrait = ({
   const getPortrait = () => (
     <div>
       <figure
-        style={{ height: size, width: size }}
+        style={{
+          height: size,
+          width: size,
+          boxShadow: "0 0 4px 0 rgb(255 255 255 / 0.3)",
+        }}
         className="ms-0 me-0 m-0 p-0.5 box-border relative flex justify-center items-center bg-neutral-700 dark:bg-black select-none rounded shadow"
       >
         <div

frontend/components/FactionIcon.tsx
@@ -28,7 +28,7 @@ const FactionIcon = ({
 
   const svg = (
     <svg
-      className={`overflow-visible mx-px my-[-2px] ${
+      className={`overflow-visible mx-0.5 my-[-2px] ${
         muted
           ? "text-neutral-500 dark:text-neutral-700"
           : "text-neutral-700 dark:text-black"

frontend/components/FactionList.module.css
@@ -1,18 +1,0 @@
-.list {
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  gap: 8px;
-  padding: 8px;
-}
-
-@media (max-width: 1400px) {
-  .list {
-    flex-grow: 1;
-    display: flex;
-    flex-direction: column;
-  }
-}
-
-.listItem {
-  display: flex;
-}

frontend/components/FactionList.tsx
@@ -3,7 +3,6 @@ import { useEffect, useState } from "react"
 import Faction from "@/classes/Faction"
 import FactionListItem from "@/components/FactionListItem"
 import { useGameContext } from "@/contexts/GameContext"
-import styles from "./FactionList.module.css"
 import Collection from "@/classes/Collection"
 
 // List of factions
@@ -33,7 +32,7 @@ const FactionList = () => {
     return (
       <div key={faction.id}>
         <div
-          className={styles.listItem}
+          className="flex"
           role="row"
           aria-label={`${faction.getName()} Faction`}
         >
@@ -45,7 +44,7 @@ const FactionList = () => {
 
   return (
     <div className="box-border h-full overflow-auto flex flex-col border border-solid border-neutral-200 dark:border-neutral-750 rounded m-4 bg-white dark:bg-neutral-600">
-      <div className={`${styles.list} shadow-inner`}>
+      <div className={`grow flex flex-col 2xl:grow-0 2xl:grid 2xl:grid-cols-2 gap-2 p-2 shadow-inner`}>
         {sortedFactions.asArray.map((faction) => getRow(faction))}
       </div>
     </div>

frontend/components/SenatorFactionInfo.tsx
@@ -2,18 +2,17 @@ import Faction from "@/classes/Faction"
 import { useGameContext } from "@/contexts/GameContext"
 import FactionLink from "@/components/FactionLink"
 import Senator from "@/classes/Senator"
-import SenatorFactList from "@/components/SenatorFactList"
-import FactionIcon from "./FactionIcon"
+import FactionIcon from "@/components/FactionIcon"
 
-interface SenatorFactionAndFactsProps {
+interface SenatorFactionInfoProps {
   senator: Senator
   selectable?: boolean
 }
 
-const SenatorFactionAndFacts = ({
+const SenatorFactionInfo = ({
   senator,
   selectable,
-}: SenatorFactionAndFactsProps) => {
+}: SenatorFactionInfoProps) => {
   const { allFactions, allTitles } = useGameContext()
 
   // Get senator-specific data
@@ -30,7 +29,7 @@ const SenatorFactionAndFacts = ({
   const getFactionDescription = () => {
     if (!faction) return null
     return (
-      <span className="ml-px">
+      <span>
         {selectable ? (
           <FactionLink faction={faction} includeIcon={true} />
         ) : (
@@ -63,9 +62,8 @@ const SenatorFactionAndFacts = ({
           </span>
         )}
       </p>
-      <SenatorFactList senator={senator} selectable={selectable} />
     </div>
   )
 }
 
-export default SenatorFactionAndFacts
+export default SenatorFactionInfo

frontend/components/SenatorLink.tsx
@@ -27,7 +27,7 @@ const SenatorLink = ({ senator }: SenatorLinkProps) => {
     return <span>{senator.displayName}</span>
 
   return (
-    <SenatorSummary senator={senator}>
+    <SenatorSummary senator={senator} inline portrait>
       <Link href="#" onClick={handleClick} sx={{ verticalAlign: "baseline" }}>
         {senator.displayName}
       </Link>

frontend/components/SenatorPortrait.module.css
@@ -1,67 +1,0 @@
-.senatorPortrait.selectable {
-  cursor: pointer;
-
-  /* Remove default button styles */
-  border: none;
-  padding: 0;
-  background-color: transparent;
-}
-
-.senatorPortrait > figure {
-  box-sizing: border-box;
-
-  position: relative;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  margin-block-start: 0;
-  margin-block-end: 0;
-  margin-inline-start: 0;
-  margin-inline-end: 0;
-}
-
-.code {
-  position: absolute;
-  z-index: 2;
-  bottom: 4px;
-  padding: 2px 6px;
-
-  font-size: 14px;
-
-  background-color: rgba(0, 0, 0, 0.4);
-  color: rgb(255, 255, 255);
-  user-select: none;
-}
-
-.imageContainer {
-  position: absolute;
-  z-index: 1;
-  overflow: hidden;
-  left: 50%;
-  top: 50%;
-  transform: translate(-50%, -50%);
-}
-
-.imageContainer img.picture {
-  position: absolute;
-  z-index: 1;
-  left: 50%;
-  top: 50%;
-}
-
-.factionLeaderPattern {
-  position: absolute;
-  height: auto;
-  width: 100%;
-  top: 0;
-  left: 0;
-}
-
-.deadIcon {
-  position: absolute;
-  right: 3px;
-  bottom: 3px;
-  z-index: 2;
-  box-sizing: border-box;
-}

commit 1256f2acb2a34b1116a2d031e3061f62a9ed544d

Fix senator portrait cursor
Fix a regression introduced by #443 that caused the cursor to no longer show a pointer when hovering over a senator portrait.

frontend/components/SenatorPortrait.tsx
@@ -222,7 +222,7 @@ const SenatorPortrait = ({
   const getPortrait = () => (
     <PortraitElement
       className={`select-none ${
-        selectable ? "border-none p-0 bg-transparent" : ""
+        selectable ? "border-none p-0 bg-transparent cursor-pointer" : ""
       }`}
       onMouseOver={handleMouseOver}
       onMouseLeave={handleMouseLeave}