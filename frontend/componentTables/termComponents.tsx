iamlogand/republic-of-rome-online

commit 6fa536249de528f3ed6e89443cdb8d1234f78fae

Add sequence of play terms (with diagram) and a term search dialog (#449)
* Add game terms related to sequence of play

* Add a search dialog

Add a search dialog that can be used to find any term. It can be opened by clicking a button at the top.

frontend/componentTables/termComponents.tsx
@@ -1,0 +1,41 @@
+import PriorConsulTerm from "@/components/terms/Term_PriorConsul"
+import FactionTerm from "@/components/terms/Term_Faction"
+import SecretTerm from "@/components/terms/Term_Secret"
+import SenatorTerm from "@/components/terms/Term_Senator"
+import MortalityPhaseTerm from "@/components/terms/Term_MortalityPhase"
+import ForumPhaseTerm from "@/components/terms/Term_ForumPhase"
+import RevolutionPhaseTerm from "@/components/terms/Term_RevolutionPhase"
+import CombatPhaseTerm from "@/components/terms/Term_CombatPhase"
+import PopulationPhaseTerm from "@/components/terms/Term_PopulationPhase"
+import RevenuePhaseTerm from "@/components/terms/Term_RevenuePhase"
+import SenatePhaseTerm from "@/components/terms/Term_SenatePhase"
+import TurnTerm from "@/components/terms/Term_Turn"
+import FactionPhaseTerm from "@/components/terms/Term_FactionPhase"
+import FinalForumPhaseTerm from "@/components/terms/Term_FinalForumPhase"
+import RomeConsulTerm from "@/components/terms/Term_RomeConsul"
+import HraoTerm from "@/components/terms/Term_Hrao"
+
+interface TermComponents {
+  [key: string]: JSX.Element
+}
+
+const termComponents: TermComponents = {
+  "Combat Phase": <CombatPhaseTerm />,
+  Faction: <FactionTerm />,
+  "Faction Phase": <FactionPhaseTerm />,
+  "Final Forum Phase": <FinalForumPhaseTerm />,
+  "Forum Phase": <ForumPhaseTerm />,
+  "HRAO: Highest Ranking Available Official": <HraoTerm />,
+  "Mortality Phase": <MortalityPhaseTerm />,
+  "Population Phase": <PopulationPhaseTerm />,
+  "Prior Consul": <PriorConsulTerm />,
+  "Revenue Phase": <RevenuePhaseTerm />,
+  "Revolution Phase": <RevolutionPhaseTerm />,
+  "Rome Consul": <RomeConsulTerm />,
+  Secret: <SecretTerm />,
+  "Senate Phase": <SenatePhaseTerm />,
+  Senator: <SenatorTerm />,
+  Turn: <TurnTerm />,
+}
+
+export default termComponents

frontend/components/actionLogs/ActionLog_TemporaryRomeConsul.tsx
@@ -56,7 +56,12 @@ const TemporaryRomeConsulActionLog = ({
           <SenatorLink senator={senator} /> of the{" "}
           <FactionLink faction={faction} /> now holds the office of{" "}
           <TermLink name="Rome Consul" displayName="Temporary Rome Consul" />,
-          making him the <TermLink name="HRAO" />.
+          making him the{" "}
+          <TermLink
+            name="HRAO: Highest Ranking Available Official"
+            displayName="HRAO"
+          />
+          .
         </p>
       )
     }

frontend/components/terms/Term_RomeConsul.tsx
@@ -18,8 +18,11 @@ const RomeConsulTerm = () => (
   >
     <p>
       The Rome Consulship is the second{" "}
-      <TermLink name="HRAO" displayName="Highest Ranking" /> Office, after the
-      Dictator (if there is one).
+      <TermLink
+        name="HRAO: Highest Ranking Available Official"
+        displayName="Highest Ranking"
+      />{" "}
+      Office, after the Dictator (if there is one).
     </p>
     <p>
       After being elected, the Rome Consul will become the Presiding Magistrate

frontend/components/ActionsArea.tsx
@@ -8,7 +8,7 @@ import ActionDataCollection from "@/data/actions.json"
 import FactionIcon from "@/components/FactionIcon"
 import { useGameContext } from "@/contexts/GameContext"
 import { useCookieContext } from "@/contexts/CookieContext"
-import ActionDialog from "@/components/ActionDialog"
+import Dialog from "@/components/Dialog"
 import ActionDataCollectionType from "@/types/Action"
 import Faction from "@/classes/Faction"
 import FactionLink from "@/components/FactionLink"
@@ -19,12 +19,17 @@ const SEQUENTIAL_PHASES = ["Forum", "Last Forum"]
 
 const ActionsArea = () => {
   const { user } = useCookieContext()
-  const { latestPhase, allPlayers, allFactions, latestActions } =
-    useGameContext()
+  const {
+    latestPhase,
+    allPlayers,
+    allFactions,
+    latestActions,
+    dialog,
+    setDialog,
+  } = useGameContext()
   const [thisFactionsPendingActions, setThisFactionsPendingActions] = useState<
     Collection<Action>
   >(new Collection<Action>())
-  const [dialogOpen, setDialogOpen] = useState<boolean>(false)
   const [faction, setFaction] = useState<Faction | null>(null)
 
   // Update faction
@@ -127,14 +132,14 @@ const ActionsArea = () => {
         <div className="my-0">
           {thisFactionsPendingActions.allIds.length > 0 && requiredAction ? (
             <div className="flex flex-col">
-              <Button variant="contained" onClick={() => setDialogOpen(true)}>
+              <Button variant="contained" onClick={() => setDialog("action")}>
                 {typedActionDataCollection[requiredAction.type]["title"]}
               </Button>
-              <ActionDialog
+              <Dialog
                 actions={thisFactionsPendingActions}
-                open={dialogOpen}
-                setOpen={setDialogOpen}
-                onClose={() => setDialogOpen(false)}
+                open={dialog == "action"}
+                setOpen={() => setDialog("action")}
+                onClose={() => setDialog(null)}
               />
             </div>
           ) : (

frontend/components/DetailSection.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef, useState } from "react"
+import { ReactNode, useCallback, useEffect, useRef, useState } from "react"
 
 import { IconButton } from "@mui/material"
 import CloseIcon from "@mui/icons-material/Close"
@@ -10,10 +10,7 @@ import { useGameContext } from "@/contexts/GameContext"
 import HraoTerm from "@/components/terms/Term_Hrao"
 import RomeConsulTerm from "@/components/terms/Term_RomeConsul"
 import SelectedDetail from "@/types/SelectedDetail"
-import PriorConsulTerm from "@/components/terms/Term_PriorConsul"
-import FactionTerm from "@/components/terms/Term_Faction"
-import SecretTerm from "@/components/terms/Term_Secret"
-import SenatorTerm from "@/components/terms/Term_Senator"
+import terms from "@/componentTables/termComponents"
 
 const BROWSING_HISTORY_LENGTH = 20
 
@@ -65,22 +62,7 @@ const DetailSection = () => {
     )
 
   // Get the component for the selected term
-  const getTermDetails = () => {
-    switch (selectedDetail.name) {
-      case "Faction":
-        return <FactionTerm />
-      case "HRAO":
-        return <HraoTerm />
-      case "Prior Consul":
-        return <PriorConsulTerm />
-      case "Rome Consul":
-        return <RomeConsulTerm />
-      case "Secret":
-        return <SecretTerm />
-      case "Senator":
-        return <SenatorTerm />
-    }
-  }
+  const getTermDetails = () => terms[selectedDetail.name] as ReactNode
 
   return (
     <div className="box-border h-full flex flex-col bg-neutral-50 dark:bg-neutral-700 rounded shadow">

frontend/components/Dialog.tsx
@@ -20,7 +20,7 @@ const dialogs: { [key: string]: React.ComponentType<any> } = {
 }
 
 // Dialog box that displays the action that the player must take
-const ActionDialog = ({
+const Dialog = ({
   open,
   setOpen,
   onClose,
@@ -43,4 +43,4 @@ const ActionDialog = ({
   }
 }
 
-export default ActionDialog
+export default Dialog

frontend/components/MetaSection.tsx
@@ -22,6 +22,7 @@ import SecretsIcon from "@/images/icons/secrets.svg"
 import AttributeFlex, { Attribute } from "@/components/AttributeFlex"
 import Collection from "@/classes/Collection"
 import SelectedDetail from "@/types/SelectedDetail"
+import Search from "@/components/Search"
 
 // Section showing meta info about the game
 const MetaSection = () => {
@@ -86,6 +87,19 @@ const MetaSection = () => {
     ]
   }
 
+  const getPhaseTerm = (phase: string) => {
+    switch (phase) {
+      case "Faction":
+        return <TermLink name="Faction Phase" />
+      case "Mortality":
+        return <TermLink name="Mortality Phase" />
+      case "Forum":
+        return <TermLink name="Forum Phase" />
+      case "Final Forum":
+        return <TermLink name="Final Forum Phase" />
+    }
+  }
+
   if (game) {
     return (
       <section className="flex flex-col-reverse lg:flex-row gap-2 align-center justify-between rounded bg-neutral-200 dark:bg-neutral-750">
@@ -134,6 +148,7 @@ const MetaSection = () => {
             </div>
           )}
           <DeveloperTools />
+          <Search />
         </div>
         <div className="self-stretch py-3 px-4 flex gap-6 justify-between bg-neutral-50 dark:bg-neutral-650 rounded shadow">
           <div className="flex flex-col gap-2 justify-around">
@@ -153,7 +168,8 @@ const MetaSection = () => {
                 <b>Game over</b>
               ) : (
                 <span>
-                  Turn {latestTurn?.index}, {latestPhase?.name} Phase
+                  Turn {latestTurn?.index},{" "}
+                  {latestPhase && getPhaseTerm(latestPhase.name)}
                 </span>
               )}
             </span>

frontend/components/Search.tsx
@@ -1,0 +1,110 @@
+import React, { useRef, useEffect, useState, useCallback } from "react"
+import Dialog from "@mui/material/Dialog"
+import IconButton from "@mui/material/IconButton"
+import TextField from "@mui/material/TextField"
+import SearchIcon from "@mui/icons-material/Search"
+import CloseIcon from "@mui/icons-material/Close"
+import { InputAdornment } from "@mui/material"
+import termComponents from "@/componentTables/termComponents"
+import { useGameContext } from "@/contexts/GameContext"
+import TermLink from "./TermLink"
+
+const Search = () => {
+  const { dialog, setDialog } = useGameContext()
+
+  const searchInputRef = useRef<HTMLInputElement>(null)
+  const [filteredTerms, setFilteredTerms] = useState<string[]>([])
+  const [query, setQuery] = useState<string>("")
+
+  const handleQueryChange = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      setQuery(event.target.value)
+    },
+    []
+  )
+
+  // Focus on search input when dialog opens
+  useEffect(() => {
+    if (searchInputRef.current) {
+      searchInputRef.current.focus()
+    }
+  }, [dialog])
+
+  // Filter terms based on the query
+  useEffect(() => {
+    const filteredTerms = Object.keys(termComponents).filter((term) =>
+      term.toLowerCase().includes(query.toLowerCase())
+    )
+    setFilteredTerms(filteredTerms)
+  }, [query])
+
+  // Clear query when dialog closes
+  useEffect(() => {
+    if (dialog !== "search") {
+      setQuery("")
+    }
+  }, [dialog])
+
+  return (
+    <>
+      <IconButton
+        aria-label="debugging tools"
+        onClick={() => setDialog("search")}
+      >
+        <SearchIcon />
+      </IconButton>
+      <Dialog
+        open={dialog === "search"}
+        onClose={() => setDialog(null)}
+        TransitionProps={{
+          onEntered: () => {
+            if (searchInputRef.current) {
+              searchInputRef.current.focus()
+            }
+          },
+        }}
+      >
+        <div className="p-6 pb-0 flex items-center gap-4">
+          <TextField
+            inputRef={searchInputRef}
+            type="text"
+            name="query"
+            value={query}
+            onChange={handleQueryChange}
+            placeholder="What are you looking for?"
+            fullWidth
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <SearchIcon />
+                </InputAdornment>
+              ),
+            }}
+          />
+        </div>
+        <div className="absolute right-2 top-2 bg-inherit">
+          <IconButton aria-label="close" onClick={() => setDialog(null)}>
+            <CloseIcon />
+          </IconButton>
+        </div>
+        <div className="m-6 h-[400px] border border-solid border-neutral-200 rounded shadow-inner overflow-y-scroll overflow-x-auto">
+          <div className="m-4 mx-6 box-border flex flex-col items-start gap-3">
+            {filteredTerms.map((term, index) => (
+              <div
+                key={index}
+                className="w-full flex items-center justify-between gap-4"
+              >
+                <span className="w-[300px]">
+                  <TermLink name={term} />
+                </span>
+                <span className="text-neutral-400">Term</span>
+              </div>
+            ))}
+          </div>
+        </div>
+      </Dialog>
+    </>
+  )
+}
+
+export default Search

commit b2d01efcc65a8d88fd4e073bcd7db3b00a970b92

Fix HRAO regression introduced by #449
Fix a regression introduced by #449 that broke the HRAO links.

frontend/componentTables/termComponents.tsx
@@ -25,7 +25,7 @@ const termComponents: TermComponents = {
   "Faction Phase": <FactionPhaseTerm />,
   "Final Forum Phase": <FinalForumPhaseTerm />,
   "Forum Phase": <ForumPhaseTerm />,
-  "HRAO: Highest Ranking Available Official": <HraoTerm />,
+  HRAO: <HraoTerm />,
   "Mortality Phase": <MortalityPhaseTerm />,
   "Population Phase": <PopulationPhaseTerm />,
   "Prior Consul": <PriorConsulTerm />,

frontend/components/actionLogs/ActionLog_TemporaryRomeConsul.tsx
@@ -58,8 +58,7 @@ const TemporaryRomeConsulActionLog = ({
           <TermLink name="Rome Consul" displayName="Temporary Rome Consul" />,
           making him the{" "}
           <TermLink
-            name="HRAO: Highest Ranking Available Official"
-            displayName="HRAO"
+            name="HRAO"
           />
           .
         </p>

frontend/components/terms/Term_RomeConsul.tsx
@@ -19,7 +19,7 @@ const RomeConsulTerm = () => (
     <p>
       The Rome Consulship is the second{" "}
       <TermLink
-        name="HRAO: Highest Ranking Available Official"
+        name="HRAO"
         displayName="Highest Ranking"
       />{" "}
       Office, after the Dictator (if there is one).