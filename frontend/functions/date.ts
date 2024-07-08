0d973bc7c48761f16e5a72bfb24a14879dca3c7a
Logan Davidson <iamlogandavidson@gmail.com>
2024-07-08 19:51:19 +0100

    Fix "0 months ago" notification issue (#492)
    * Fix "0 months ago" notification issue
    
    Fix an issue where it was possible for notifications in the UI to show as "0 months ago".
    
    * Make `formatElapsedDate` more performant.
    
    Refactor to make the `formatElapsedDate` slightly more performant.

--- frontend/functions/date.ts
+++ frontend/functions/date.ts
@@ -18,36 +18,30 @@ export const formatElapsedDate = (date: Date, timezone: string) => {
   const elapsed = now.getTime() - zonedDate.getTime()
 
   const seconds = Math.floor(elapsed / 1000)
-  if (seconds < 60) {
+  const minutes = Math.floor(seconds / 60)
+  if (minutes < 1) {
     return "Now"
   }
-
-  const minutes = Math.floor(seconds / 60)
-  if (minutes < 60) {
+  const hours = Math.floor(minutes / 60)
+  if (hours < 1) {
     return `${minutes} ${minutes === 1 ? "min" : "mins"} ago`
   }
-
-  const hours = Math.floor(minutes / 60)
-  if (hours < 24) {
+  const days = Math.floor(hours / 24)
+  if (days < 1) {
     return `${hours} ${hours === 1 ? "hour" : "hours"} ago`
   }
-
-  const days = Math.floor(hours / 24)
-  if (days < 7) {
+  const weeks = Math.floor(days / 7)
+  if (weeks < 1) {
     return `${days} ${days === 1 ? "day" : "days"} ago`
   }
-
-  const weeks = Math.floor(days / 7)
-  if (weeks < 4) {
+  const months = Math.floor(days / 30)
+  if (months < 1) {
     return `${weeks} ${weeks === 1 ? "week" : "weeks"} ago`
   }
-
-  const months = Math.floor(days / 30)
-  if (months < 12) {
+  const years = Math.floor(days / 365)
+  if (years < 1) {
     return `${months} ${months === 1 ? "month" : "months"} ago`
   }
-
-  const years = Math.floor(days / 365)
   return `${years} ${years === 1 ? "year" : "years"} ago`
 }
 
