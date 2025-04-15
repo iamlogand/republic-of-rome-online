c887911479b1dc3ad6105b675858f6807d141ee6
Logan Davidson <iamlogandavidson@gmail.com>
2025-04-15 22:03:25 +0100

    feat: add more info related to signing in (#550)

--- frontend/components/GoogleLogin.tsx
+++ frontend/components/GoogleLogin.tsx
@@ -1,4 +1,11 @@
+import { Roboto } from "next/font/google"
 import Image from "next/image"
+
+const roboto = Roboto({
+  weight: "500",
+  subsets: ["latin"],
+  display: "swap",
+})
 
 interface GoogleLoginProps {
   onClick: () => void
@@ -6,14 +13,24 @@ interface GoogleLoginProps {
 
 const GoogleLogin = ({ onClick }: GoogleLoginProps) => {
   return (
-    <button onClick={onClick}>
-      <Image
-        src="\googleLogin.svg"
-        height={42}
-        width={200}
-        alt="Continue with Google"
-      />
-    </button>
+    <div>
+      <button
+        onClick={onClick}
+        className="flex h-10 items-center gap-2.5 rounded border border-[#747775] px-3 py-2.5"
+      >
+        <Image
+          src="/googleIcon.png"
+          height={20}
+          width={20}
+          alt="Continue with Google"
+        />
+        <span
+          className={`text-sm font-[500] text-[#1F1F1F] ${roboto.className}`}
+        >
+          Continue with Google
+        </span>
+      </button>
+    </div>
   )
 }
 
