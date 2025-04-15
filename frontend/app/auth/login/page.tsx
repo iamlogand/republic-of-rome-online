2e5604c0413956c84037cd3e9509065bcb8e9f28
Logan Davidson <iamlogandavidson@gmail.com>
2025-04-15 22:01:25 +0100

    feat: add more info related to signing in

--- frontend/app/auth/login/page.tsx
+++ frontend/app/auth/login/page.tsx
@@ -62,10 +62,63 @@ const LoginPage = () => {
             <p>Signing in...</p>
           </div>
         ) : (
-          <>
-            <h2 className="mb-4 text-lg font-bold">Sign in</h2>
+          <div className="flex max-w-[800px] flex-col gap-4">
+            <h2 className="text-xl">Sign in</h2>
+            <p>
+              Click the button below to sign in or create a new account for{" "}
+              <i>Republic of Rome Online</i>.
+            </p>
             <GoogleLogin onClick={handleGoogleLogin} />
-          </>
+            <div className="mt-4 flex flex-col gap-4 rounded bg-neutral-100 px-6 py-4 text-sm">
+              <h3 className="text-lg">About</h3>
+              <p>
+                Currently it&apos;s only possible to create an account by
+                signing in with Google. This requires that you share the
+                following information with roronline.com:
+              </p>
+              <ul className="flex list-disc flex-col gap-1 pl-8">
+                <li>Name</li>
+                <li>Email address</li>
+                <li>Profile picture</li>
+              </ul>
+              <p>How this information is used:</p>
+              <ul className="flex list-disc flex-col gap-1 pl-8">
+                <li>
+                  Your first name is used to generate your initial username, but
+                  you can change it at any time. Your username is only visible
+                  to other players if you host or join a game.
+                </li>
+                <li>
+                  Your email address is not shared with other players. However,
+                  it may be used to contact you regarding gameplay issues, to
+                  request feedback, or to inform you about updates and changes
+                  to the project.
+                </li>
+                <li>
+                  Your profile picture may be used in the future to personalize
+                  your account and display your identity to other players during
+                  the game. This feature will be <i>opt-in</i> and never enabled
+                  by default.
+                </li>
+              </ul>
+              <p>
+                You may delete your account and all associated data at any time.
+              </p>
+              <p>
+                All data is handled responsibly and never shared with third
+                parties. Since this is an open-source project, transparency and
+                community trust are priorities. If you have any concerns, feel
+                free to reach out or review the codebase on{" "}
+                <a
+                  className="text-blue-600 hover:underline"
+                  href="https://github.com/iamlogand/republic-of-rome-online"
+                >
+                  GitHub
+                </a>
+                .
+              </p>
+            </div>
+          </div>
         )}
       </div>
     </>
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -14,6 +14,14 @@ const Home = async () => {
           >
             The Republic of Rome
           </a>
+          . To see what&apos;s been completed and what&apos;s still in progress,
+          check out the regularly updated feature checklist{" "}
+          <a
+            className="text-blue-600 hover:underline"
+            href="https://github.com/iamlogand/republic-of-rome-online?tab=readme-ov-file#feature-checklist-early-republic-scenario"
+          >
+            here
+          </a>
           .
         </p>
         <p className="mb-4">
@@ -27,36 +35,40 @@ const Home = async () => {
           . It is licensed under the MIT license, so anyone is free to use,
           modify, or build upon it for any purpose.
         </p>
-        <h2 className="py-4 text-xl">About the game</h2>
-        <p className="mb-4 italic">
-          The Republic of Rome is an abstraction of over 250 years of history.
-          It simulates the politics of the Roman Senate during the republic. The
-          players take the part of various factions vying for the control of the
-          senate. They control the various powerful families of the time, who
-          compete for state offices, military command, economic concessions and
-          new adherents. To win the player must get their faction to become the
-          most powerful in Rome. While doing this, however, a balance must be
-          maintained. A hostile world situation, and the vagaries of the public
-          of Rome means that the players must also cooperate so that Rome
-          herself doesn&apos;t go down under this pressure. If Rome does not
-          last, neither does the senate, and all players lose!
-        </p>
-        <p className="mb-4 italic">
-          Players make proposals to the Senate which other players then vote on.
-          A player&apos;s ability to make proposals is determined by which
-          Offices his/her Senators hold. A player&apos;s influence in votes is
-          determined by the number of Senators they have recruited and the level
-          of influence those Senators have obtained. Proposals may include
-          assigning Senators to govern provinces (generating revenue),
-          recruiting an army to fight an external foe, addressing the concerns
-          of the Roman people, assigning offices or prosecuting previous office
-          holders. Players have to co-operate to overcome the various threats
-          that the game sends against Rome (wars, famine, unrest, bankruptcy)
-          whilst working to build their own Senators&apos; and Generals&apos;
-          positions and undermine that of their opponents. A powerful General or
-          an influential Senator may become Emperor (thus winning the game) but
-          equally may suddenly fall to the plague or an assassin&apos;s blade.
-        </p>
+        <div className="mt-8 flex flex-col gap-4 rounded bg-neutral-100 px-6 py-4 text-sm">
+          <h2 className="text-lg">About the game</h2>
+          <p>
+            The Republic of Rome is an abstraction of over 250 years of history.
+            It simulates the politics of the Roman Senate during the republic.
+            The players take the part of various factions vying for the control
+            of the senate. They control the various powerful families of the
+            time, who compete for state offices, military command, economic
+            concessions and new adherents. To win the player must get their
+            faction to become the most powerful in Rome. While doing this,
+            however, a balance must be maintained. A hostile world situation,
+            and the vagaries of the public of Rome means that the players must
+            also cooperate so that Rome herself doesn&apos;t go down under this
+            pressure. If Rome does not last, neither does the senate, and all
+            players lose!
+          </p>
+          <p>
+            Players make proposals to the Senate which other players then vote
+            on. A player&apos;s ability to make proposals is determined by which
+            Offices his/her Senators hold. A player&apos;s influence in votes is
+            determined by the number of Senators they have recruited and the
+            level of influence those Senators have obtained. Proposals may
+            include assigning Senators to govern provinces (generating revenue),
+            recruiting an army to fight an external foe, addressing the concerns
+            of the Roman people, assigning offices or prosecuting previous
+            office holders. Players have to co-operate to overcome the various
+            threats that the game sends against Rome (wars, famine, unrest,
+            bankruptcy) whilst working to build their own Senators&apos; and
+            Generals&apos; positions and undermine that of their opponents. A
+            powerful General or an influential Senator may become Emperor (thus
+            winning the game) but equally may suddenly fall to the plague or an
+            assassin&apos;s blade.
+          </p>
+        </div>
       </div>
     </>
   )
