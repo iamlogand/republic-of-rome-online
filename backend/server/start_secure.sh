83855a12a6a4afee00d3d2fc8931b4c45e93162c
Logan Davidson <iamlogandavidson@gmail.com>
2025-04-27 11:31:17 +0100

    build: switch to Let's Encrypt with Certbot

--- backend/server/start_secure.sh
+++ backend/server/start_secure.sh
@@ -9,9 +9,6 @@ chown -R www-data:www-data /app/staticfiles
 # Migrate
 python manage.py migrate
 
-# Install Nginx
-apt-get update && apt-get install -y nginx
-
 # Remove the default Nginx configuration file
 rm /etc/nginx/sites-enabled/default
 
@@ -21,8 +18,28 @@ cp server/nginx_secure.conf /etc/nginx/sites-enabled/
 # Check and create directory if not exists
 [ ! -d "/etc/nginx/ssl/" ] && mkdir -p /etc/nginx/ssl/
 
-# Download certificates
-python server/s3_download.py
+# Set domain and email
+DOMAIN="api.roronline.com"
+EMAIL="iamlogandavidson@gmail.com"
 
-# Start Nginx and Daphne
-service nginx start && daphne rorsite.asgi:application --bind 0.0.0.0 --port 8000
+# Start nginx before certbot
+service nginx start
+
+# Request certificate
+if [ ! -f "/etc/letsencrypt/live/$DOMAIN/fullchain.pem" ]; then
+    certbot certonly --standalone --non-interactive --agree-tos --email $EMAIL -d $DOMAIN
+fi
+
+# Reload nginx to pick up certs
+service nginx reload
+
+# Start Daphne
+daphne rorsite.asgi:application --bind 0.0.0.0 --port 8000
+
+# Start certbot renew in the background every 12 hours
+while true
+do
+    certbot renew --quiet
+    service nginx reload
+    sleep 43200  # 12 hours
+done &
