e65bf7680d704c53a60661bb0b215f6bc81808a6
Logan Davidson <iamlogandavidson@gmail.com>
2025-05-25 21:21:59 +0100

    fix: use redis hostname specified in env var

--- backend/rorsite/settings.py
+++ backend/rorsite/settings.py
@@ -199,7 +199,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("redis", 6379)],
+            "hosts": [(os.getenv("REDIS_HOSTNAME"), 6379)],
         },
     },
 }
