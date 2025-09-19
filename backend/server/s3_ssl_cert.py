770f0400a79fa7036909ebde817f3e1370434b1f
Logan Davidson <iamlogandavidson@gmail.com>
2025-09-19 08:07:41 +0100

    fix: find and replace mistake

--- backend/server/s3_ssl_cert.py
+++ backend/server/s3_ssl_cert.py
@@ -12,7 +12,7 @@ s3 = boto3.client(
     "s3",
     aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
     aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
-    location_name=os.getenv("AWS_DEFAULT_location"),
+    region_name=os.getenv("AWS_DEFAULT_REGION"),
 )
 
 
