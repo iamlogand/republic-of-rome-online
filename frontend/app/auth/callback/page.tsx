6acd39fd20fa23c75fa569f7d5f90c2980a128d9
Logan Davidson <iamlogandavidson@gmail.com>
2025-11-16 16:58:01 +0000

    feat: post sign in redirect

--- frontend/app/auth/callback/page.tsx
+++ frontend/app/auth/callback/page.tsx
@@ -1,9 +1,36 @@
 "use client"
 
-import { redirect } from "next/navigation"
+import { useEffect } from "react"
+
+import { useRouter } from "next/navigation"
+
+import NavBar from "@/components/NavBar"
 
 const AuthCallbackPage = () => {
-  redirect("/")
+  const router = useRouter()
+
+  useEffect(() => {
+    const redirectUrl = localStorage.getItem("post_auth_redirect")
+
+    localStorage.removeItem("post_auth_redirect")
+
+    if (redirectUrl && redirectUrl.startsWith("/")) {
+      router.push(redirectUrl)
+    } else {
+      router.push("/")
+    }
+  }, [router])
+
+  return (
+    <>
+      <NavBar visible />
+      <div className="px-4 py-4">
+        <div className="flex justify-center">
+          <p>Redirecting...</p>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default AuthCallbackPage
--- frontend/app/not-found.tsx
+++ frontend/app/not-found.tsx
@@ -1,27 +1,31 @@
 "use client"
 
+import NavBar from "@/components/NavBar"
 import { useAppContext } from "@/contexts/AppContext"
 
 const NotFound = () => {
   const { user } = useAppContext()
 
   return (
-    <div className="flex w-full flex-col items-center gap-4 pt-16">
-      <h2>404 - Page Not Found</h2>
-      <p className="text-center text-neutral-600">
-        {user ? (
-          <>
-            Either this page doesn&apos;t exist, or you don&apos;t have
-            permission to access it
-          </>
-        ) : (
-          <>
-            Either this page doesn&apos;t exist, or you need to sign in to
-            access it
-          </>
-        )}
-      </p>
-    </div>
+    <>
+      <NavBar visible></NavBar>
+      <div className="flex w-full flex-col items-center gap-4 pt-16">
+        <h2>404 - Page Not Found</h2>
+        <p className="text-center text-neutral-600">
+          {user ? (
+            <>
+              Either this page doesn&apos;t exist, or you don&apos;t have
+              permission to access it
+            </>
+          ) : (
+            <>
+              Either this page doesn&apos;t exist, or you need to sign in to
+              access it
+            </>
+          )}
+        </p>
+      </div>
+    </>
   )
 }
 export default NotFound
