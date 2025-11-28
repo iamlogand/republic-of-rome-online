9d93bb9c427a266ed075400a95754325f5a851bf
Logan Davidson <iamlogandavidson@gmail.com>
2025-11-28 08:27:52 +0000

    test: naval battle tests

--- backend/tests/test_combat/test_naval_battle.py
+++ backend/tests/test_combat/test_naval_battle.py
@@ -1,12 +1,11 @@
-from typing import List
 import pytest
 from rorapp.classes.random_resolver import FakeRandomResolver
-from rorapp.models import Campaign, Faction, Fleet, Game, Legion, Senator, War
+from rorapp.models import Campaign, Game, Fleet, Senator, War
 from rorapp.effects.meta.effect_executor import execute_effects_and_manage_actions
 
 
 @pytest.fixture
-def land_campaign(basic_game: Game):
+def naval_campaign(basic_game: Game):
     game = basic_game
 
     game.phase = Game.Phase.COMBAT
@@ -16,16 +15,16 @@ def land_campaign(basic_game: Game):
 
     war = War.objects.create(
         game=game,
-        name="1st Gallic War",
-        series_name="Gallic",
+        name="1st Punic War",
+        series_name="Punic",
         index=0,
         land_strength=10,
-        fleet_support=0,
-        naval_strength=0,
+        fleet_support=5,
+        naval_strength=10,
         disaster_numbers=[13],
-        standoff_numbers=[15],
-        spoils=20,
-        location="Cisalpine Gaul",
+        standoff_numbers=[11, 14],
+        spoils=35,
+        location="Sicilia",
         status=War.Status.ACTIVE,
     )
 
@@ -40,15 +39,15 @@ def land_campaign(basic_game: Game):
 
 
 @pytest.mark.django_db
-def test_land_battle_victory(land_campaign: Campaign):
+def test_naval_battle_victory(naval_campaign: Campaign):
 
-    campaign = land_campaign
-    game = land_campaign.game
-    commander = land_campaign.commander
+    campaign = naval_campaign
+    game = naval_campaign.game
+    commander = naval_campaign.commander
     assert commander is not None
 
     for i in range(1, 11):
-        Legion.objects.create(game=game, number=i, campaign=campaign)
+        Fleet.objects.create(game=game, number=i, campaign=campaign)
 
     random_resolver = FakeRandomResolver()
     random_resolver.dice_rolls = [18]
@@ -59,62 +58,32 @@ def test_land_battle_victory(land_campaign: Campaign):
     execute_effects_and_manage_actions(game.id, random_resolver)
 
     # Assert
-    surviving_legion_count = Legion.objects.filter(game=game).count()
-    assert surviving_legion_count == 10
+    surviving_fleet_count = Fleet.objects.filter(game=game).count()
+    assert surviving_fleet_count == 10
     game.refresh_from_db()
     assert game.unrest == 2
     commander.refresh_from_db()
     assert commander.alive == True
     assert commander.popularity == 5
-    assert commander.location == "Rome"
-    assert Campaign.objects.filter(game=game).exists() == False
-
-
-@pytest.mark.django_db
-def test_land_battle_stalemate(land_campaign: Campaign):
-
-    campaign = land_campaign
-    game = land_campaign.game
-    commander = land_campaign.commander
-    assert commander is not None
-
-    for i in range(1, 11):
-        Legion.objects.create(game=game, number=i, campaign=campaign)
-
-    random_resolver = FakeRandomResolver()
-    random_resolver.dice_rolls = [5]
-    random_resolver.casualty_order = []
-    random_resolver.mortality_chits = []
-
-    # Act
-    execute_effects_and_manage_actions(game.id, random_resolver)
-
-    # Assert
-    surviving_legion_count = Legion.objects.filter(game=game).count()
-    assert surviving_legion_count == 6
-    game.refresh_from_db()
-    assert game.unrest == 3
-    commander.refresh_from_db()
-    assert commander.alive == True
-    assert commander.popularity == -2
+    assert commander.location == "Sicilia"
     assert Campaign.objects.filter(game=game).exists() == True
     campaign.refresh_from_db()
     assert campaign.commander == commander
 
 
 @pytest.mark.django_db
-def test_land_battle_defeat(land_campaign: Campaign):
+def test_naval_battle_stalemate(naval_campaign: Campaign):
 
-    campaign = land_campaign
-    game = land_campaign.game
-    commander = land_campaign.commander
+    campaign = naval_campaign
+    game = naval_campaign.game
+    commander = naval_campaign.commander
     assert commander is not None
 
-    for i in range(1, 7):
-        Legion.objects.create(game=game, number=i, campaign=campaign)
+    for i in range(1, 11):
+        Fleet.objects.create(game=game, number=i, campaign=campaign)
 
     random_resolver = FakeRandomResolver()
-    random_resolver.dice_rolls = [4]
+    random_resolver.dice_rolls = [7]
     random_resolver.casualty_order = []
     random_resolver.mortality_chits = []
 
@@ -122,27 +91,29 @@ def test_land_battle_defeat(land_campaign: Campaign):
     execute_effects_and_manage_actions(game.id, random_resolver)
 
     # Assert
-    surviving_legion_count = Legion.objects.filter(game=game).count()
-    assert surviving_legion_count == 2
+    surviving_fleet_count = Fleet.objects.filter(game=game).count()
+    assert surviving_fleet_count == 8
     game.refresh_from_db()
-    assert game.unrest == 5
+    assert game.unrest == 3
     commander.refresh_from_db()
-    assert commander.alive == False
+    assert commander.alive == True
+    assert commander.popularity == 0
+    assert commander.location == "Sicilia"
     assert Campaign.objects.filter(game=game).exists() == True
     campaign.refresh_from_db()
-    assert campaign.commander == None
+    assert campaign.commander == commander
 
 
 @pytest.mark.django_db
-def test_land_battle_defeat_total(land_campaign: Campaign):
+def test_naval_battle_defeat(naval_campaign: Campaign):
 
-    campaign = land_campaign
-    game = land_campaign.game
-    commander = land_campaign.commander
+    campaign = naval_campaign
+    game = naval_campaign.game
+    commander = naval_campaign.commander
     assert commander is not None
 
-    for i in range(1, 7):
-        Legion.objects.create(game=game, number=i, campaign=campaign)
+    for i in range(1, 11):
+        Fleet.objects.create(game=game, number=i, campaign=campaign)
 
     random_resolver = FakeRandomResolver()
     random_resolver.dice_rolls = [3]
@@ -153,8 +124,36 @@ def test_land_battle_defeat_total(land_campaign: Campaign):
     execute_effects_and_manage_actions(game.id, random_resolver)
 
     # Assert
-    surviving_legion_count = Legion.objects.filter(game=game).count()
-    assert surviving_legion_count == 0
+    surviving_fleet_count = Fleet.objects.filter(game=game).count()
+    assert surviving_fleet_count == 9
+    game.refresh_from_db()
+    assert game.unrest == 5
+    commander.refresh_from_db()
+    assert commander.alive == False
+
+
+@pytest.mark.django_db
+def test_naval_battle_total_defeat(naval_campaign: Campaign):
+
+    campaign = naval_campaign
+    game = naval_campaign.game
+    commander = naval_campaign.commander
+    assert commander is not None
+
+    for i in range(1, 6):
+        Fleet.objects.create(game=game, number=i, campaign=campaign)
+
+    random_resolver = FakeRandomResolver()
+    random_resolver.dice_rolls = [1]
+    random_resolver.casualty_order = []
+    random_resolver.mortality_chits = []
+
+    # Act
+    execute_effects_and_manage_actions(game.id, random_resolver)
+
+    # Assert
+    surviving_fleet_count = Fleet.objects.filter(game=game).count()
+    assert surviving_fleet_count == 0
     game.refresh_from_db()
     assert game.unrest == 5
     commander.refresh_from_db()
@@ -163,18 +162,18 @@ def test_land_battle_defeat_total(land_campaign: Campaign):
 
 
 @pytest.mark.django_db
-def test_land_battle_standoff(land_campaign: Campaign):
+def test_naval_battle_standoff(naval_campaign: Campaign):
 
-    campaign = land_campaign
-    game = land_campaign.game
-    commander = land_campaign.commander
+    campaign = naval_campaign
+    game = naval_campaign.game
+    commander = naval_campaign.commander
     assert commander is not None
 
     for i in range(1, 11):
-        Legion.objects.create(game=game, number=i, campaign=campaign)
+        Fleet.objects.create(game=game, number=i, campaign=campaign)
 
     random_resolver = FakeRandomResolver()
-    random_resolver.dice_rolls = [15]
+    random_resolver.dice_rolls = [11]
     random_resolver.casualty_order = []
     random_resolver.mortality_chits = []
 
@@ -182,29 +181,29 @@ def test_land_battle_standoff(land_campaign: Campaign):
     execute_effects_and_manage_actions(game.id, random_resolver)
 
     # Assert
-    surviving_legion_count = Legion.objects.filter(game=game).count()
-    assert surviving_legion_count == 7
+    surviving_fleet_count = Fleet.objects.filter(game=game).count()
+    assert surviving_fleet_count == 7
     game.refresh_from_db()
     assert game.unrest == 3
     commander.refresh_from_db()
     assert commander.alive == True
-    assert commander.popularity == -1
-    assert commander.location == "Cisalpine Gaul"
+    assert commander.popularity == 0
+    assert commander.location == "Sicilia"
     assert Campaign.objects.filter(game=game).exists() == True
     campaign.refresh_from_db()
     assert campaign.commander == commander
 
 
 @pytest.mark.django_db
-def test_land_battle_disaster(land_campaign: Campaign):
+def test_naval_battle_disaster(naval_campaign: Campaign):
 
-    campaign = land_campaign
-    game = land_campaign.game
-    commander = land_campaign.commander
+    campaign = naval_campaign
+    game = naval_campaign.game
+    commander = naval_campaign.commander
     assert commander is not None
 
     for i in range(1, 11):
-        Legion.objects.create(game=game, number=i, campaign=campaign)
+        Fleet.objects.create(game=game, number=i, campaign=campaign)
 
     random_resolver = FakeRandomResolver()
     random_resolver.dice_rolls = [13]
@@ -215,14 +214,14 @@ def test_land_battle_disaster(land_campaign: Campaign):
     execute_effects_and_manage_actions(game.id, random_resolver)
 
     # Assert
-    surviving_legion_count = Legion.objects.filter(game=game).count()
-    assert surviving_legion_count == 5
+    surviving_fleet_count = Fleet.objects.filter(game=game).count()
+    assert surviving_fleet_count == 5
     game.refresh_from_db()
     assert game.unrest == 4
     commander.refresh_from_db()
     assert commander.alive == True
-    assert commander.popularity == -2
-    assert commander.location == "Cisalpine Gaul"
+    assert commander.popularity == 0
+    assert commander.location == "Sicilia"
     assert Campaign.objects.filter(game=game).exists() == True
     campaign.refresh_from_db()
     assert campaign.commander == commander
