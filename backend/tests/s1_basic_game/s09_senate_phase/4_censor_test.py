9119f27ac44fc5e35a841525fefc7fd555fd73c9
Logan Davidson <iamlogandavidson@gmail.com>
2026-03-08 19:22:33 +0000

    fix: add null checks

--- backend/tests/s1_basic_game/s09_senate_phase/4_censor_test.py
+++ backend/tests/s1_basic_game/s09_senate_phase/4_censor_test.py
@@ -25,6 +25,7 @@ def test_censor_elected(senate_censor_game: Game, resolver: FakeRandomResolver):
     senators[2].add_status_item(Senator.StatusItem.MAJOR_CORRUPT)
     senators[2].save()
 
+    assert julius.faction_id is not None
     faction = Faction.objects.get(id=julius.faction_id)
     ElectCensorAction().execute(game.id, faction.id, {"Censor": cornelius.id}, FakeRandomResolver())
 
@@ -59,6 +60,7 @@ def test_censor_election_defeated(senate_censor_game: Game, resolver: FakeRandom
     senators[2].add_title(Senator.Title.PRIOR_CONSUL)
     senators[2].save()
 
+    assert julius.faction_id is not None
     faction = Faction.objects.get(id=julius.faction_id)
     ElectCensorAction().execute(game.id, faction.id, {"Censor": cornelius.id}, FakeRandomResolver())
 
