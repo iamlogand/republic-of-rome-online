68a9f2dcb2633e4faf41c4402927100fe7831a92
Logan Davidson <iamlogandavidson@gmail.com>
2026-06-21 11:28:24 +0100

    ci: speed up backend CI
    - switch to SQLLite in-memory DB
    - use pytest-xdist parallelism
    - use native runner instead of Docker Compose

---
+++ backend/rorsite/settings_test.py
@@ -0,0 +1,16 @@
+from .settings import *
+
+SECRET_KEY = "test-secret-key-not-for-production"
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": ":memory:",
+    }
+}
+
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels.layers.InMemoryChannelLayer",
+    }
+}
