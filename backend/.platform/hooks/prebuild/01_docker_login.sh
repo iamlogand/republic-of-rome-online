0b07a6e7833d2e62877dd854dc6cefa8f1cc0033
Logan Davidson <iamlogandavidson@gmail.com>
2026-06-25 23:10:51 +0100

    fix: read Docker Hub credentials from SSM in EB prebuild hook

--- backend/.platform/hooks/prebuild/01_docker_login.sh
+++ backend/.platform/hooks/prebuild/01_docker_login.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-DOCKER_HUB_USERNAME=$(/opt/elasticbeanstalk/bin/get-config environment -k DOCKER_HUB_USERNAME)
-DOCKER_HUB_TOKEN=$(/opt/elasticbeanstalk/bin/get-config environment -k DOCKER_HUB_TOKEN)
+DOCKER_HUB_USERNAME=$(aws ssm get-parameter --region eu-west-2 --name /roronline/DOCKER_HUB_USERNAME --query Parameter.Value --output text)
+DOCKER_HUB_TOKEN=$(aws ssm get-parameter --region eu-west-2 --name /roronline/DOCKER_HUB_TOKEN --with-decryption --query Parameter.Value --output text)
 
 echo "$DOCKER_HUB_TOKEN" | docker login --username "$DOCKER_HUB_USERNAME" --password-stdin
