31d8985ab3801466c5c837e57e36b909b9105ea0
Joseph Rajewski <83741749+paladin-devops@users.noreply.github.com>
2026-06-29 15:54:50 -0400

    fix: fix province name max length in migration

--- backend/rorapp/migrations/0056_province.py
+++ backend/rorapp/migrations/0056_province.py
@@ -15,7 +15,7 @@ class Migration(migrations.Migration):
             name='Province',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=30)),
+                ('name', models.CharField(max_length=20)),
                 ('developed', models.BooleanField(default=False)),
                 ('game', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='provinces', to='rorapp.game')),
             ],
