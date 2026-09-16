7be8b234c32c0fc8c558026137b524ee65dc7a00
Logan Davidson <iamlogandavidson@gmail.com>
2026-09-16 12:46:30 +0100

    feat: enable use of feature flags (#850)
    * feat: enable use of feature flags
    
    * docs: tweak
    
    * fix: revert accidental import removal

--- backend/rorapp/effects/lay_down_command.py
+++ backend/rorapp/effects/lay_down_command.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+
 from rorapp.classes.random_resolver import RandomResolver
 from rorapp.effects.meta.effect_base import EffectBase
 from rorapp.game_state.game_state_snapshot import GameStateSnapshot
@@ -12,6 +14,7 @@ class LayDownCommandEffect(EffectBase):
         return (
             game_state.game.phase == Game.Phase.REVOLUTION
             and game_state.game.sub_phase == Game.SubPhase.CIVIL_WAR_DECLARATION
+            and not settings.FEATURE_FLAGS.get("civil_war")
             and any(c.land_victory for c in game_state.campaigns)
         )
 
